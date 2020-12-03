10d679d26ccaa58c792fc5e765bfe0514ec61f83
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2020-12-03 17:26:45 +0100

    add unit tets for enumerate & quadratic equation

---
+++ tests/unit_tests/core/utils_enumerate.cpp
@@ -0,0 +1,44 @@
+/** Detray library, part of the ACTS project (R&D line)
+ * 
+ * (c) 2020 CERN for the benefit of the ACTS project
+ * 
+ * Mozilla Public License Version 2.0
+ */
+
+#include <vector>
+
+#include "utils/enumerate.hpp"
+#include "tests/common/test_defs.hpp"
+
+#include <gtest/gtest.h>
+
+
+using namespace detray;
+
+// This tests the convenience enumeration function
+TEST(utils, enumerate_function)
+{
+
+    struct uint_holder {
+        unsigned int ui = 0;
+    };
+
+    std::vector<uint_holder> seq = { {0}, {1}, {2}, {3}, {4}, {5}};
+
+    using container_type_iter = decltype(std::begin(std::declval<std::vector<uint_holder>>()));
+
+    for (auto [i, v] : enumerate(seq)){
+        ASSERT_EQ(i, v.ui);
+    }
+
+}
+
+// Google Test can be run manually from the main() function
+// or, it can be linked to the gtest_main library for an already
+// set-up main() function primed to accept Google Test test cases.
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
---
+++ tests/unit_tests/core/utils_quadratic_equation.cpp
@@ -0,0 +1,37 @@
+/** Detray library, part of the ACTS project (R&D line)
+ * 
+ * (c) 2020 CERN for the benefit of the ACTS project
+ * 
+ * Mozilla Public License Version 2.0
+ */
+
+#include <vector>
+
+#include "utils/quadratic_equation.hpp"
+#include "tests/common/test_defs.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace detray;
+
+// This tests the convenience quadratic equation struct
+TEST(utils, quad_equation)
+{
+    quadratic_equation<scalar> qe = {{2., 5., -3.}};
+    auto solution = qe();
+
+    ASSERT_EQ(std::get<0>(solution), 2);
+    ASSERT_NEAR(std::get<1>(solution)[0], -3., 1e-5);
+    ASSERT_NEAR(std::get<1>(solution)[1], 0.5, 1e-5);
+
+}
+
+// Google Test can be run manually from the main() function
+// or, it can be linked to the gtest_main library for an already
+// set-up main() function primed to accept Google Test test cases.
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
