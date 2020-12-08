a193764075d007a9359503145ecde869e80de3a0
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2020-12-08 14:03:32 +0100

    hopefully better formatted shell script

--- tests/scripts/run_benchmarks.sh
+++ tests/scripts/run_benchmarks.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-export PWD=`pwd`
+export PWD=$(pwd)
 cd ${GITHUB_WORKSPACE}
-export LASTCOMMIT =`git log -n1 | head -n1 | cut -b 8-14`
+export LASTCOMMIT=$(git log -n1 | head -n1 | cut -b 8-14)
 echo "Benchmark nalysis of commit ${LASTCOMMIT}"
 cd ${PWD}
 
-export DETRAY_TEST_DATA_DIR=${GITHUB_WORKSPACE}/tests/data;
+export DETRAY_TEST_DATA_DIR=${GITHUB_WORKSPACE}/tests/data
 
 echo "Running core.benchmarks ..."
 ./bin/core_masks --benchmark_out=core_masks.csv --benchmark_out_format=csv
 
 echo "Running eigen.benchmarks ..."
 ./bin/eigen_intersect_surfaces --benchmark_out=eigen_intersect_surfaces.csv --benchmark_out_format=csv
-./bin/eigen_intersect_all  --benchmark_out=eigen_intersect_all.csv --benchmark_out_format=csv
+./bin/eigen_intersect_all --benchmark_out=eigen_intersect_all.csv --benchmark_out_format=csv
 
 echo "Install components for benchmark analysis ..."
 pip3 install matplotlib numpy pandas
-
-
