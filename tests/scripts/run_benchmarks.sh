6daf3d29b6b3f192208fc0754c860be78f4651f0
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2020-12-08 13:49:06 +0100

    Extract commit hash and write files

--- tests/scripts/run_benchmarks.sh
+++ tests/scripts/run_benchmarks.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+export lastcommit =`git log -n1 | head -n1 | cut -b 8-14`
+echo "Benchmark nalysis of commit ${lastcommit}"
+
 echo "Running core.benchmarks ..."
-./bin/core_masks
+./bin/core_masks --benchmark_out=core_masks.csv --benchmark_out_format=csv
 
 echo "Running eigen.benchmarks ..."
-./bin/eigen_intersect_surfaces
-./bin/eigen_intersect_all
+./bin/eigen_intersect_surfaces --benchmark_out=eigen_intersect_surfaces.csv --benchmark_out_format=csv
+./bin/eigen_intersect_all  --benchmark_out=eigen_intersect_all.csv --benchmark_out_format=csv
 
-echo "Install componetns for benchmark analysis ..."
-python3 --version 
-pip3 install matplotlib
+echo "Install components for benchmark analysis ..."
+pip3 install matplotlib numpy pandas
 
 
