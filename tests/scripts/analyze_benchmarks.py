f2b05d10b770b8631784b546dac568f697672a92
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2020-12-11 12:26:08 +0100

    change axis of benchmark plots

--- tests/scripts/analyze_benchmarks.py
+++ tests/scripts/analyze_benchmarks.py
@@ -4,9 +4,13 @@ import matplotlib.pyplot as plt
 
 benchmarks = pd.read_csv('benchmarks_history.csv').groupby(['name'])
 for bench in benchmarks :
-    gbenchmarks = bench[1].groupby(['group']);
+    gbenchmarks = bench[1].groupby(['group']);        
+    maxl = 0
     for gbench in gbenchmarks :                
-        plt.plot(gbench[1]['commit'], gbench[1]['cpu_time'], marker="o", label=gbench[0])
+        maxl = max(maxl, len(gbench[1]))
+    for gbench in gbenchmarks :
+        curl = len(gbench[1])
+        plt.plot(range(maxl-curl, maxl), gbench[1]['cpu_time'], marker="o", label=gbench[0])
     plt.legend()
     plt.savefig(bench[0] + '.png', format='png')
     plt.cla()
