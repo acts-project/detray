e56368aae19df75bec0eb0150394a7cef526d234
Joana Niermann <joana.niermann@cern.ch>
2021-06-01 16:06:04 +0200

    Integrated the vc_array plugin

---
+++ tests/unit_tests/vc/vc_array_cylinder_intersection.cpp
@@ -0,0 +1,9 @@
+/** Detray library, part of the ACTS project (R&D line)
+ * 
+ * (c) 2020 CERN for the benefit of the ACTS project
+ * 
+ * Mozilla Public License Version 2.0
+ */
+
+#include "plugins/algebra/vc_array_container.hpp"
+#include "tests/common/tools_cylinder_intersection.inl"
