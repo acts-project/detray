495bd8a162a59138526e5b3d33d3b2bab2a0e18d
Joana Niermann <joana.niermann@cern.ch>
2021-06-03 20:54:01 +0200

    Update type names

--- plugins/algebra/include/detray/vc/include/plugins/algebra/vc_array_definitions.hpp
+++ plugins/algebra/include/detray/vc/include/plugins/algebra/vc_array_definitions.hpp
@@ -7,16 +7,16 @@ namespace detray {
     using scalar = algebra::scalar;
 
     template <typename value_type, unsigned int kDIM>
-    using darray = algebra::array_t<value_type, kDIM>;
+    using darray = algebra::array_s<value_type, kDIM>;
 
     template <typename value_type>
-    using dvector = algebra::vector_t<value_type>;
+    using dvector = algebra::vector_v<value_type>;
 
     template <typename key_type, typename value_type>
-    using dmap = algebra::map_t<key_type, value_type>;
+    using dmap = algebra::map_s<key_type, value_type>;
 
     template< class... types>
-    using dtuple = algebra::tuple_t<types ...>;
+    using dtuple = algebra::tuple_s<types ...>;
 
     namespace getter = algebra::getter;
     namespace vector = algebra::vector;
