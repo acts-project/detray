98359fb232f3c012b977d72e3e4c6eb06933c531
beomki-yeo <beomki.yeo@gmail.com>
2021-09-20 10:07:18 -0700

    minor fix

--- plugins/algebra/include/detray/smatrix/include/plugins/algebra/smatrix_definitions.hpp
+++ plugins/algebra/include/detray/smatrix/include/plugins/algebra/smatrix_definitions.hpp
@@ -5,21 +5,19 @@
 
 namespace detray {
 
-    using scalar = algebra::scalar;
+using scalar = algebra::scalar;
 
-    template <typename value_type, unsigned int kDIM>
-    using darray = std::array<value_type, kDIM>;
+template <typename value_type, unsigned int kDIM>
+using darray = algebra::array_s<value_type, kDIM>;
 
-    template <typename value_type>
-    using dvector = vecmem::vector<value_type>;
-    
-    template <typename key_type, typename value_type>
-    using dmap = algebra::map_s<key_type, value_type>;
+template <typename value_type> using dvector = algebra::vector_s<value_type>;
 
-    template< class... types>
-    using dtuple = algebra::tuple_s<types ...>;
+template <typename key_type, typename value_type>
+using dmap = algebra::map_s<key_type, value_type>;
 
-    namespace getter = algebra::getter;
-    namespace vector = algebra::vector;
+template <class... types> using dtuple = algebra::tuple_s<types...>;
 
-} //namespace detray
+namespace getter = algebra::getter;
+namespace vector = algebra::vector;
+
+} // namespace detray
