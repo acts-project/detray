304e2d9bfd4a800ff8891bc1849cbc20545d0d15
beomki-yeo <beomki.yeo@gmail.com>
2021-09-21 09:28:34 -0700

    fix

--- tests/benchmarks/core/benchmark_grids.cpp
+++ tests/benchmarks/core/benchmark_grids.cpp
@@ -5,11 +5,14 @@
  * Mozilla Public License Version 2.0
  */
 
+// detray test
+#include "tests/common/test_defs.hpp"
+
+// detray core
 #include "grids/axis.hpp"
 #include "grids/grid2.hpp"
 #include "grids/populator.hpp"
 #include "grids/serializer2.hpp"
-#include "tests/common/test_defs.hpp"
 #include "utils/indexing.hpp"
 
 #include <benchmark/benchmark.h>
