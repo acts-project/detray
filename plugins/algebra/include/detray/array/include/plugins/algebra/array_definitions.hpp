a006ab9f4eb9b6ffbb27a0f0a05e058822f07ab6
beomki-yeo <beomki.yeo@gmail.com>
2021-09-23 15:42:55 -0700

    use plugin algebra vector

--- plugins/algebra/include/detray/array/include/plugins/algebra/array_definitions.hpp
+++ plugins/algebra/include/detray/array/include/plugins/algebra/array_definitions.hpp
@@ -1,7 +1,6 @@
 
 #include "algebra/definitions/std_array.hpp"
 #include "vecmem/containers/jagged_vector.hpp"
-#include "vecmem/containers/vector.hpp"
 
 #define ALGEBRA_PLUGIN array
 
@@ -13,7 +12,7 @@ template <typename value_type, unsigned int kDIM>
 using darray = std::array<value_type, kDIM>;
 
 template <typename value_type>
-using dvector = vecmem::vector<value_type>;
+using dvector = algebra::vector_s<value_type>;
 
 template <typename value_type>
 using djagged_vector = vecmem::jagged_vector<value_type>;
