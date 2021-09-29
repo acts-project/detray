00c880f2593edfd2c0e15e14fadb3eb8f6b3aefc
beomki-yeo <beomki.yeo@gmail.com>
2021-09-29 04:12:53 -0700

    add qualifer to invalid value

--- core/include/definitions/invalid_values.hpp
+++ core/include/definitions/invalid_values.hpp
@@ -10,24 +10,24 @@
 #include <limits>
 
 template <typename T>
-inline T invalid_value() {
+DETRAY_HOST_DEVICE inline T invalid_value() {
     return T::invalid_value();
 }
 
 // specialization for int
 template <>
-inline int invalid_value() {
+DETRAY_HOST_DEVICE inline int invalid_value() {
     return std::numeric_limits<int>::max();
 }
 
 // specialization for unsigned int
 template <>
-inline unsigned int invalid_value() {
+DETRAY_HOST_DEVICE inline unsigned int invalid_value() {
     return std::numeric_limits<unsigned int>::max();
 }
 
 // specialization for long unsigned int
 template <>
-inline long unsigned int invalid_value() {
+DETRAY_HOST_DEVICE inline long unsigned int invalid_value() {
     return std::numeric_limits<long unsigned int>::max();
 }
