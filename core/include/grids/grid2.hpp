38c8d6ffba26d5f3de07e6af6ab64a660833410b
beomki-yeo <beomki.yeo@gmail.com>
2021-10-15 13:43:23 -0700

    make it a bit better

--- core/include/grids/grid2.hpp
+++ core/include/grids/grid2.hpp
@@ -62,7 +62,8 @@ class grid2 {
           const bare_value m_invalid = invalid_value<bare_value>())
         : _axis_p0(axis_p0),
           _axis_p1(axis_p1),
-#if defined(array) || defined(eigen) || defined(smatrix)
+#if defined(vc_array)
+#else
           _data_serialized(&mr),
 #endif
           _populator(m_invalid) {
@@ -82,7 +83,8 @@ class grid2 {
           const bare_value m_invalid = invalid_value<bare_value>())
         : _axis_p0(std::move(axis_p0)),
           _axis_p1(std::move(axis_p1)),
-#if defined(array) || defined(eigen)
+#if defined(vc_array)
+#else
           _data_serialized(&mr),
 #endif
           _populator(m_invalid) {
