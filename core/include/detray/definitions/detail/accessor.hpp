91a2e64e36ba8bcb2765323dc88d7260fde1ba1d
beomki-yeo <beomki.yeo@gmail.com>
2021-11-05 13:03:47 -0700

    using namespace for get

--- core/include/detray/definitions/detail/accessor.hpp
+++ core/include/detray/definitions/detail/accessor.hpp
@@ -26,7 +26,7 @@ namespace vtuple = std;
 
 namespace detail {
 
-/** tuple size
+/** tuple size accessor
  */
 template <class T>
 struct tuple_size;
@@ -35,111 +35,11 @@ template <template <typename...> class tuple_type, class... value_types>
 struct tuple_size<tuple_type<value_types...>>
     : std::integral_constant<std::size_t, sizeof...(value_types)> {};
 
-/** thrust tuple accessor
+/** get function accessor
+ *
  */
-
-template <unsigned int index, typename... value_types>
-DETRAY_HOST_DEVICE inline constexpr decltype(auto) get(
-    const thrust::tuple<value_types...> &tuple) {
-    return thrust::get<index>(tuple);
-}
-
-template <unsigned int index, typename... value_types>
-DETRAY_HOST_DEVICE inline constexpr decltype(auto) get(
-    thrust::tuple<value_types...> &tuple) {
-    return thrust::get<index>(tuple);
-}
-
-/** thrust pair accessor
- */
-
-template <unsigned int index, typename... value_types>
-DETRAY_HOST_DEVICE inline constexpr decltype(auto) get(
-    const thrust::pair<value_types...> &pair) {
-    return thrust::get<index>(pair);
-}
-
-template <unsigned int index, typename... value_types>
-DETRAY_HOST_DEVICE inline constexpr decltype(auto) get(
-    thrust::pair<value_types...> &pair) {
-    return thrust::get<index>(pair);
-}
-
-/** std tuple accessor
- */
-
-template <unsigned int index, typename... value_types>
-DETRAY_HOST_DEVICE inline constexpr decltype(auto) get(
-    const std::tuple<value_types...> &tuple) {
-    return std::get<index>(tuple);
-}
-
-template <unsigned int index, typename... value_types>
-DETRAY_HOST_DEVICE inline constexpr decltype(auto) get(
-    std::tuple<value_types...> &tuple) {
-    return std::get<index>(tuple);
-}
-
-/** In case the index is not known at compile time. */
-template <unsigned int current_index = 0, typename... value_types>
-DETRAY_HOST_DEVICE inline decltype(auto) get(
-    const std::tuple<value_types...> &tuple, unsigned int index) {
-    // Check index against constexpr
-    if (current_index == index) {
-        return std::get<current_index>(tuple);
-    }
-    // Next tuple index
-    if constexpr (current_index <
-                  std::tuple_size_v<std::tuple<value_types...>> - 1) {
-        return get<current_index + 1>(tuple, index);
-    }
-}
-
-template <unsigned int current_index = 0, typename... value_types>
-DETRAY_HOST_DEVICE inline decltype(auto) get(std::tuple<value_types...> &tuple,
-                                             unsigned int index) {
-    // Check index against constexpr
-    if (current_index == index) {
-        return std::get<current_index>(tuple);
-    }
-    // Next tuple index
-    if constexpr (current_index <
-                  std::tuple_size_v<std::tuple<value_types...>> - 1) {
-        return get<current_index + 1>(tuple, index);
-    }
-}
-
-/** std pair accessor
- */
-
-template <unsigned int index, typename... value_types>
-DETRAY_HOST_DEVICE inline constexpr decltype(auto) get(
-    const std::pair<value_types...> &pair) {
-    return std::get<index>(pair);
-}
-
-template <unsigned int index, typename... value_types>
-DETRAY_HOST_DEVICE inline constexpr decltype(auto) get(
-    std::pair<value_types...> &pair) {
-    return std::get<index>(pair);
-}
-
-/** std array accessor
- */
-
-template <unsigned int index, typename value_type, unsigned int N>
-DETRAY_HOST_DEVICE inline constexpr decltype(auto) get(
-    std::array<value_type, N> &array) {
-    return std::get<index>(array);
-}
-
-template <unsigned int index, typename value_type, unsigned int N>
-DETRAY_HOST_DEVICE inline constexpr decltype(auto) get(
-    const std::array<value_type, N> &array) {
-    return std::get<index>(array);
-}
-
-/** Put new link types here */
+using std::get;
+using thrust::get;
 
 }  // namespace detail
 }  // namespace detray
