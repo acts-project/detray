60c9fe071006c157106ede87fbd8cdc74bb0cee3
Joana Niermann <joana.niermann@cern.ch>
2022-03-28 01:09:29 +0200

    pass constant b field

--- core/include/detray/tools/rk_stepper.hpp
+++ core/include/detray/tools/rk_stepper.hpp
@@ -33,7 +33,8 @@ class rk_stepper final : public base_stepper<track_t, constraint_t> {
     using context_type = typename magnetic_field_t::context_type;
 
     DETRAY_HOST_DEVICE
-    rk_stepper(magnetic_field_t& mag_field) : _magnetic_field(mag_field) {}
+    rk_stepper(const magnetic_field_t& mag_field)
+        : _magnetic_field(mag_field) {}
 
     struct state : public base_type::state {
         DETRAY_HOST_DEVICE
