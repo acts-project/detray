0c2343b68882e77ef1acdaab393a006aecc9d0f4
Joana Niermann <joana.niermann@cern.ch>
2022-04-09 02:19:14 +0200

    Call policy update only once

--- core/include/detray/propagator/navigation_policies.hpp
+++ core/include/detray/propagator/navigation_policies.hpp
@@ -8,6 +8,8 @@
 #pragma once
 
 // detray definitions
+#include <climits>
+
 #include "detray/definitions/qualifiers.hpp"
 #include "detray/propagator/base_actor.hpp"
 
@@ -49,13 +51,19 @@ struct guided_navigation : actor {
 
 namespace step {
 
-/// During guided navigation only the next surface should be re-evaluated. This
-/// maps to the 'high trust' level in the navigator
+/// Default navigation update policy for the steppers: If a constraint has been
+/// hit, lower the trustlevel to 'fair trust', otherwise stay in 'high trust'.
+/// The reasoning is, that the track state might have changed much when a
+/// constraint was triggered.
 struct default_policy : actor {
 
-    struct state_type {
+    /// State of the policy keeps track of the path length
+    struct policy_state {
         scalar _path_length{0};
+        scalar _tolerance{std::numeric_limits<scalar>::epsilon()};
     };
+
+    using state_type = policy_state;
 
     /// Sets the navigation trust level to 'no trust'
     ///
@@ -63,17 +71,26 @@ struct default_policy : actor {
     /// @param prop_state state of the propagation
     template <typename propagator_state_t>
     DETRAY_HOST_DEVICE inline void operator()(
-        const state_type & /*pol_state*/,
-        propagator_state_t &prop_state) const {
+        state_type &pol_state, propagator_state_t &prop_state) const {
 
         const auto &stepping = prop_state._stepping;
+
+        // This is run after the navigator update - do nothing
+        if (std::abs(pol_state._path_length - stepping.path_length()) <
+            pol_state._tolerance) {
+            return;
+        }
+
+        // Update path length when the stepper advanced the track state
+        pol_state._path_length = stepping.path_length();
+
         auto &navigation = prop_state._navigation;
 
-        // Decide final step size and inform navigator
         // Not a severe change to track state expected
         if (std::abs(stepping.step_size()) <
             std::abs(
                 stepping.constraints().template size<>(stepping.direction()))) {
+            // Re-evaluate only next candidate
             navigation.set_high_trust();
         }
         // Step size hit a constraint - the track state was probably changed a
