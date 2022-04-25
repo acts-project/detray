7bbdf4091f4e4bbcab6662140fa2d3d47d6a212f
Joana Niermann <joana.niermann@cern.ch>
2022-04-25 15:18:37 +0200

    adapt benchmark to different policies

--- tests/benchmarks/cuda/benchmark_propagator_cuda.cpp
+++ tests/benchmarks/cuda/benchmark_propagator_cuda.cpp
@@ -54,6 +54,7 @@ void fill_tracks(vecmem::vector<free_track_parameters> &tracks,
     }
 }
 
+template <typename stepper_policy_t>
 static void BM_PROPAGATOR_CPU(benchmark::State &state) {
 
     // Create the toy geometry
@@ -67,13 +68,13 @@ static void BM_PROPAGATOR_CPU(benchmark::State &state) {
     field_type B_field(B);
 
     // Create RK stepper
-    rk_stepper_type s(B_field);
+    rk_stepper_type<stepper_policy_t> s(B_field);
 
     // Create navigator
     navigator_host_type n(det);
 
     // Create propagator
-    propagator_host_type p(std::move(s), std::move(n));
+    propagator_host_type<stepper_policy_t> p(std::move(s), std::move(n));
 
     for (auto _ : state) {
 
@@ -88,7 +89,7 @@ static void BM_PROPAGATOR_CPU(benchmark::State &state) {
         for (auto &track : tracks) {
 
             // Create the propagator state
-            propagator_host_type::state p_state(track);
+            propagator_host_type<stepper_policy_t>::state p_state(track);
 
             // Run propagation
             p.propagate(p_state);
@@ -96,6 +97,7 @@ static void BM_PROPAGATOR_CPU(benchmark::State &state) {
     }
 }
 
+template <typename stepper_policy_t>
 static void BM_PROPAGATOR_CUDA(benchmark::State &state) {
 
     // Create the toy geometry
@@ -129,11 +131,24 @@ static void BM_PROPAGATOR_CUDA(benchmark::State &state) {
         copy.setup(candidates_buffer);
 
         // Run the propagator test for GPU device
-        propagator_benchmark(det_data, tracks_data, candidates_buffer);
+        propagator_benchmark<stepper_policy_t>(det_data, tracks_data,
+                                               candidates_buffer);
     }
 }
 
-BENCHMARK(BM_PROPAGATOR_CPU)->RangeMultiplier(2)->Range(8, 256);
-BENCHMARK(BM_PROPAGATOR_CUDA)->RangeMultiplier(2)->Range(8, 256);
+BENCHMARK(BM_PROPAGATOR_CPU<always_init>)->RangeMultiplier(2)->Range(8, 256);
+BENCHMARK(BM_PROPAGATOR_CUDA<always_init>)->RangeMultiplier(2)->Range(8, 256);
+BENCHMARK(BM_PROPAGATOR_CPU<stepper_default_policy>)
+    ->RangeMultiplier(2)
+    ->Range(8, 256);
+BENCHMARK(BM_PROPAGATOR_CUDA<stepper_default_policy>)
+    ->RangeMultiplier(2)
+    ->Range(8, 256);
+BENCHMARK(BM_PROPAGATOR_CPU<stepper_rk_correction_policy>)
+    ->RangeMultiplier(2)
+    ->Range(8, 256);
+BENCHMARK(BM_PROPAGATOR_CUDA<stepper_rk_correction_policy>)
+    ->RangeMultiplier(2)
+    ->Range(8, 256);
 
 BENCHMARK_MAIN();
