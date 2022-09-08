31c3dc3683a737bff59a151677afcb29bfa690b6
beomki-yeo <beomki.yeo@gmail.com>
2022-09-07 19:54:05 -0700

    Edit lincense properly

--- core/include/detray/coordinates/cartesian2.hpp
+++ core/include/detray/coordinates/cartesian2.hpp
@@ -1,6 +1,6 @@
-/** Algebra plugins library, part of the ACTS project
+/** Detray library, part of the ACTS project
  *
- * (c) 2020-2022 CERN for the benefit of the ACTS project
+ * (c) 2022 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
