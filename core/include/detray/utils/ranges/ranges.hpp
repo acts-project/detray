d666aaf1de5e4e797e7813dd555196b2f658fd34
Joana Niermann <joana.niermann@cern.ch>
2022-09-11 13:37:01 +0200

    make subrange const correct and nisc changes

--- core/include/detray/utils/ranges/ranges.hpp
+++ core/include/detray/utils/ranges/ranges.hpp
@@ -6,6 +6,7 @@
  */
 #pragma once
 
+#include <cassert>
 #include <type_traits>
 
 #include "detray/definitions/qualifiers.hpp"
@@ -14,174 +15,142 @@
 
 namespace detray::ranges {
 
-/// Shorthand to test a type for being iterable.
-template <typename T>
-inline constexpr bool is_iterable_v = detail::iterable<T>::value;
-
 /// @brief Detail interface of an iterable type.
 ///
 /// Base case: The type is not iterable
 template <typename T, typename = void>
-struct range : public std::false_type {};
+struct range : public std::false_type {
+    using type = void;
+};
 
 /// @brief Check if a type is iterable and extract iterator types.
 ///
-/// A range has a begin() and an end() member function, thus becoming iterable.
-/// It wraps some kind of iterable: this can be a container which is either
-/// owned by the range or not, or it can be another instace of @c iterable, e.g.
-/// another range.
+/// A range has a begin() and an end() member function, which is guaranteed by
+/// 'is_iterable'.
 template <typename T>
-struct range<T, std::enable_if_t<is_iterable_v<T>, void>>
+struct range<T,
+             std::enable_if_t<detail::iterable<std::decay_t<T>>::value, void>>
     : public std::true_type {
 
-    /// Extract begin() and end() functions from [detray] containers
-    using iterable_type = detail::iterable<T>;
-
-    /// Compliance with std::ranges typedefs
-    using iterator_t = typename iterable_type::iterator_type;
-    using const_iterator_t = typename iterable_type::const_iterator_type;
-    using range_value_t =
-        typename detail::iterator_traits<iterator_t>::value_type;
-    using range_size_t = std::size_t;
-    using range_difference_t =
-        typename detail::iterator_traits<iterator_t>::difference_type;
-    using range_reference_t =
-        typename detail::iterator_traits<iterator_t>::reference;
-    using range_const_reference_t = const range_reference_t;
-    using range_rvalue_reference_t = range_value_t&&;
+    /// Provide begin() and end() functions of e.g. [detray] containers
+    using type = detail::iterable<std::decay_t<T>>;
 };
 
-/// Check if a type @c T is a range and get the @c begin function
+/// Check if a type 'T' is a range and get the 'begin()'/'end()' functions from
+/// the  @c iterable interface
+/// @{
 template <class T>
-auto begin(T&& iterable) {
-    return range<T>::iterable_type::begin(iterable);
+constexpr auto begin(T&& iterable) noexcept {
+    return range<std::decay_t<T>>::type::begin(iterable);
 }
 
-/// Check if a type @c T is a range and get the @c end function
 template <class T>
-auto end(T&& iterable) {
-    return range<T>::iterable_type::end(iterable);
+constexpr auto end(T&& iterable) noexcept {
+    return range<std::decay_t<T>>::type::end(iterable);
 }
 
-/// Check if a type @c T is a range and get the @c rbegin function
 template <class T>
-auto rbegin(T&& iterable) {
-    return range<T>::iterable_type::rbegin(iterable);
+constexpr auto cbegin(T&& iterable) noexcept {
+    return range<std::decay_t<T>>::type::cbegin(iterable);
 }
 
-/// Check if a type @c T is a range and get the @c rend function
 template <class T>
-auto rend(T&& iterable) {
-    return range<T>::iterable_type::rend(iterable);
+constexpr auto cend(T&& iterable) noexcept {
+    return range<std::decay_t<T>>::type::cend(iterable);
 }
 
-/// Check if a type @c T is a range and get the @c cbegin function
 template <class T>
-auto cbegin(T&& iterable) {
-    return range<T>::iterable_type::cbegin(iterable);
+constexpr auto size(T&& iterable) noexcept {
+    return std::distance(detray::ranges::begin(std::forward<T>(iterable)),
+                         detray::ranges::end(std::forward<T>(iterable)));
 }
+/// @}
 
-/// Check if a type @c T is a range and get the @c cend function
+/// Compliance with std::ranges typedefs,
+/// @see https://en.cppreference.com/w/cpp/ranges/iterator_t
+/// @{
 template <class T>
-auto cend(T&& iterable) {
-    return range<T>::iterable_type::cend(iterable);
-}
+using iterator_t = decltype(detray::ranges::begin(std::declval<T&>()));
 
-/// Check if a type @c T is a range and get the @c crbegin function
 template <class T>
-auto crbegin(T&& iterable) {
-    return range<T>::iterable_type::crbegin(iterable);
-}
+using const_iterator_t = iterator_t<const T>;
 
-/// Check if a type @c T is a range and get the @c crend function
 template <class T>
-auto crend(T&& iterable) {
-    return range<T>::iterable_type::crend(iterable);
-}
+using sentinel_t = decltype(detray::ranges::end(std::declval<T&>()));
 
-}  // namespace detray::ranges
+template <class T>
+using range_size_t = decltype(detray::ranges::size(std::declval<T&>()));
+
+template <class T>
+using range_difference_t = typename std::iterator_traits<
+    detray::ranges::iterator_t<T>>::difference_type;
+
+template <class T>
+using range_value_t =
+    typename std::iterator_traits<detray::ranges::iterator_t<T>>::value_type;
+
+template <class T>
+using range_reference_t =
+    typename std::iterator_traits<detray::ranges::iterator_t<T>>::reference;
+
+template <class T>
+using range_const_reference_t = const range_reference_t<T>;
+
+template <class T>
+using range_rvalue_reference_t = std::decay_t<range_value_t<T>>&&;
+/// @}
 
 // https://en.cppreference.com/w/cpp/ranges/view
-namespace detray::views {
 
 /// Tags a type as a view
 struct base_view {};
 
 /// Defines a detray 'view'
 template <typename view_impl_t>
-struct view_interface {
+struct view_interface : public base_view {
 
-    view_interface() = default;
+    // static_assert(range<view_impl_t>::value, "Only ranges can satisfy the
+    // 'view' interface");
 
-    DETRAY_HOST_DEVICE
-    auto empty() const { return (size() == 0); }
-
-    DETRAY_HOST_DEVICE
-    auto data() const { return m_impl_ptr->begin(); }
+    constexpr view_interface() = default;
 
     DETRAY_HOST_DEVICE
-    auto size() const {
-        return std::distance(m_impl_ptr->end(), m_impl_ptr->begin());
+    constexpr auto empty() const noexcept -> bool {
+        return (m_impl_ptr->begin() == m_impl_ptr->end());
     }
 
     DETRAY_HOST_DEVICE
-    auto front() { return *(m_impl_ptr->begin()); }
+    constexpr auto data() const noexcept { return m_impl_ptr->begin(); }
 
     DETRAY_HOST_DEVICE
-    auto back() { return *(m_impl_ptr->end()); }
+    constexpr auto size() const noexcept {
+        return std::distance(m_impl_ptr->begin(), m_impl_ptr->end());
+    }
+
+    DETRAY_HOST_DEVICE
+    constexpr auto front() noexcept { return *(m_impl_ptr->begin()); }
+
+    DETRAY_HOST_DEVICE
+    constexpr auto back() noexcept { return *(std::prev(m_impl_ptr->end())); }
 
     /// @return element at position i, relative to iterator range.
     DETRAY_HOST_DEVICE
-    inline auto& operator[](const dindex i) {
+    constexpr inline auto& operator[](const dindex i) {
         return *(m_impl_ptr->begin() + i);
     }
 
     /// @return element at position i, relative to iterator range - const
     DETRAY_HOST_DEVICE
-    inline const auto& operator[](const dindex i) const {
+    constexpr inline const auto& operator[](const dindex i) const {
         return *(m_impl_ptr->begin() + i);
     }
 
-    const view_impl_t* m_impl_ptr{static_cast<const view_impl_t*>(this)};
+    view_impl_t* const m_impl_ptr{static_cast<view_impl_t* const>(this)};
 };
 
-/*template<template<typename> class T, typename iterable_t>
-inline constexpr bool enable_view
-    = std::is_base_of_v<base_view, T<iterable_t>> or
-        std::is_base_of_v<view_interface<T<iterable_t>>, T<iterable_t>>;
+template <template <typename> class T, typename iterable_t>
+inline constexpr bool enable_view =
+    std::is_base_of_v<base_view, T<iterable_t>> or
+    std::is_base_of_v<view_interface<T<iterable_t>>, T<iterable_t>>;
 
-// https://en.cppreference.com/w/cpp/ranges/owning_view
-template<template range_t>
-struct owning_view<range_t> : public view_interface<owning_view<range_t>> {
-    owning_view() = delete;
-
-    owning_view(range_t && data) : m_data(std::move(data)) {}
-
-    /// @returns start position, which is at the wrapped value.
-    template<typename... Args>
-    DETRAY_HOST_DEVICE
-     inline auto begin() const -> iterator_type {
-        return ranges::begin(m_data);
-    }
-
-    /// @returns end position, which is the position behind the wrapped value.
-    DETRAY_HOST_DEVICE
-    static inline auto end() const -> iterator_type {
-        return ranges::end(m_data);
-    }
-
-    range_t m_data;
-};
-
-template<template range_t>
-struct non_owning_view<range_t>
-    : public view_interface<non_owning_view<range_t>> {
-
-    non_owning_view() = default;
-
-    non_owning_view(const range_t & data) : m_data(data) {}
-
-    const range_t & m_data;
-};*/
-
-}  // namespace detray::views
+}  // namespace detray::ranges
--- tests/unit_tests/core/utils_ranges.cpp
+++ tests/unit_tests/core/utils_ranges.cpp
@@ -12,34 +12,35 @@
 
 // detray core
 #include "detray/utils/ranges/subrange.hpp"
+#include "detray/utils/ranges/views.hpp"
 
 using namespace detray;
 
 // This tests the convenience range_enumeration function: single
-/*TEST(utils, range) {
+/*TEST(utils, views_iota_single) {
 
     dindex check = 0;
     dindex single = 7;
-    for (auto i : sequence(single)) {
+    for (auto i : detray::ranges::iota_view(single)) {
         check += i;
     }
     ASSERT_EQ(check, single);
 }
 
 // This tests the convenience range_enumeration function: range
-TEST(utils, sequence_range) {
+TEST(utils, views_iota) {
 
     darray<dindex, 2> range = {2, 7};
     std::vector<dindex> reference = {2, 3, 4, 5, 6, 7};
     std::vector<dindex> check = {};
-    for (auto i : sequence(range)) {
+    for (auto i : detray::ranges::iota_view(range)) {
         check.push_back(i);
     }
     ASSERT_EQ(check, reference);
-}
+}*/
 
 // This tests the convenience enumeration function
-TEST(utils, enumerate) {
+/*TEST(utils, enumerate) {
 
     struct uint_holder {
         unsigned int ui = 0;
@@ -52,18 +53,35 @@ TEST(utils, enumerate) {
     }
 }*/
 
-// This tests the restricted iterator
-TEST(utils, views_subrange) {
-    size_t begin = 1;
-    size_t end = 4;
+// Test the subrange implementation
+TEST(utils, ranges_subrange) {
+
+    std::size_t begin = 1;
+    std::size_t end = 4;
+    std::array<std::size_t, 2> interval{begin, end};
 
     dvector<int> seq = {0, 1, 2, 3, 4, 5};
 
-    size_t i = 1;
-    for (const auto &v :
-         views::subrange(seq, std::array<size_t, 2>{begin, end})) {
-        ASSERT_NE(v, 5);
+    // general tests
+    auto sr = detray::ranges::subrange_view(seq, interval);
+
+    ASSERT_TRUE(detray::ranges::range<decltype(sr_c)>::value);
+    ASSERT_EQ(sr[1], seq[begin + 1]);
+    ASSERT_EQ(sr.size(), 3UL);
+    ASSERT_EQ(sr.front(), 1UL);
+    ASSERT_EQ(sr.back(), 3UL);
+
+    // non-const iteration
+    std::size_t i = 1;
+    for (const auto &v : detray::ranges::subrange_view(seq, interval)) {
+        ASSERT_NE(v, 4);
         ASSERT_EQ(v, seq[i++]);
-        std::cout << v << std::endl;
+    }
+
+    // const iteration
+    const dvector<int> seq_c(seq);
+    i = 1;
+    for (const auto &v : detray::ranges::subrange_view(seq_c, interval)) {
+        ASSERT_EQ(v, seq[i++]);
     }
 }
