1e072ca79f6dd353a7a3451d163f7bec429c5048
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2022-11-18 21:46:10 +0100

    feat: json detector io (#356)
    This PR adds the necessary infrastructure to load detectors geometries from ACTS/Core to detray.
    
    It adds the nlohmann library, and and io layer for payload objects that contain all the information that will allow us to construct the detray::detector representing an Acts::Experimental::Detector object to the same detail.
    
    It will be followed by a PR adding the actual construction code which then will be templated into the algebra-plugin and vecmem backend.

---
+++ io/json/include/detray/io/json_grids_io.hpp
@@ -0,0 +1,97 @@
+/** Detray library, part of the ACTS project (R&D line)
+ *
+ * (c) 2022 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#pragma once
+
+#include <array>
+#include <optional>
+#include <vector>
+
+#include "detray/io/io_payload.hpp"
+#include "detray/io/json_defs.hpp"
+
+namespace detray {
+
+void to_json(nlohmann::json& j, const axis_payload& a) {
+    j["label"] = static_cast<unsigned int>(a.label);
+    j["bounds"] = static_cast<unsigned int>(a.bounds);
+    j["binning"] = static_cast<unsigned int>(a.binning);
+    j["edges"] = a.edges;
+    j["bins"] = a.bins;
+}
+
+void from_json(const nlohmann::json& j, axis_payload& a) {
+    a.binning = static_cast<axis_payload::axis_binning>(j["binning"]);
+    a.bounds = static_cast<axis_payload::axis_bounds>(j["bounds"]);
+    a.label = static_cast<axis_payload::axis_label>(j["label"]);
+    a.edges = j["edges"].get<std::vector<real_io>>();
+    a.bins = j["bins"];
+}
+
+void to_json(nlohmann::json& j, const grid_payload& g) {
+    nlohmann::json jaxes;
+    for (const auto& a : g.axes) {
+        jaxes.push_back(a);
+    }
+    j["axes"] = jaxes;
+    j["entries"] = g.entries;
+}
+
+void from_json(const nlohmann::json& j, grid_payload& g) {
+    nlohmann::json jaxes = j["axes"];
+    for (auto jax : jaxes) {
+        axis_payload a = jax;
+        g.axes.push_back(a);
+    }
+    g.entries = j["entries"];
+}
+
+void to_json(nlohmann::json& j, const single_object_payload& so) {
+    j = so.link;
+}
+
+void from_json(const nlohmann::json& j, single_object_payload& so) {
+    so.link = j;
+}
+
+void to_json(nlohmann::json& j, const grid_objects_payload& g) {
+    j["grid"] = g.grid;
+    if (g.transform.has_value()) {
+        j["transform"] = g.transform.value();
+    }
+}
+
+void from_json(const nlohmann::json& j, grid_objects_payload& g) {
+    g.grid = j["grid"];
+    if (j.find("transform") != j.end()) {
+        g.transform = j["transform"];
+    }
+}
+
+void to_json(nlohmann::json& j, const links_payload& l) {
+    nlohmann::json js;
+    for (const auto& so : l.single_links) {
+        js.push_back(so);
+    }
+    j["single_links"] = js;
+    if (l.grid_links.has_value()) {
+        j["grid_links"] = l.grid_links.value();
+    }
+}
+
+void from_json(const nlohmann::json& j, links_payload& l) {
+    nlohmann::json jsl = j["single_links"];
+    for (auto jl : jsl) {
+        single_object_payload sl = jl;
+        l.single_links.push_back(sl);
+    }
+    if (j.find("grid_links") != j.end()) {
+        l.grid_links = j["grid_links"];
+    }
+}
+
+}  // namespace detray
---
+++ io/json/include/detray/io/json_material_io.hpp
@@ -0,0 +1,29 @@
+/** Detray library, part of the ACTS project (R&D line)
+ *
+ * (c) 2022 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#pragma once
+
+#include <array>
+
+#include "detray/io/io_payload.hpp"
+#include "detray/io/json_algebra_io.hpp"
+#include "detray/io/json_defs.hpp"
+
+/// @brief  The detray JSON I/O is written in such a way that it
+/// can read/write ACTS files that are written with the Detray
+/// JSON I/O extension
+namespace detray {
+
+void to_json(nlohmann::json& j, const material_slab_payload& m) {
+    j = m.slab;
+}
+
+void from_json(const nlohmann::json& j, material_slab_payload& m) {
+    m.slab = j.get<std::array<real_io, 5u> >();
+}
+
+}  // namespace detray
