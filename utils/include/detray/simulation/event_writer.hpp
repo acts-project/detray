2c9dc64b3817430fdbb379997ec82eef54248b9c
beomki-yeo <beomki.yeo@gmail.com>
2022-11-20 17:15:31 -0800

    minor fix

--- utils/include/detray/simulation/event_writer.hpp
+++ utils/include/detray/simulation/event_writer.hpp
@@ -123,8 +123,8 @@ struct event_writer : actor {
                 surface.mask(), bound_params, writer_state.m_meas_smearer);
 
             meas.measurement_id = writer_state.m_hit_count;
-            meas.local_key = "unknown";
             meas.geometry_id = hit.geometry_id;            
+            meas.local_key = "unknown";
             meas.local0 = local[0];
             meas.local1 = local[1];
             auto stddev_0 = writer_state.m_meas_smearer.stddev[0];
