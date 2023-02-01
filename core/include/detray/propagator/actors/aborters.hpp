e89b0b7c068c97d6efb8efee6ac9dfe9b4c30dfe
Joana Niermann <joana.niermann@cern.ch>
2023-02-01 19:00:11 +0100

    Implement target_aborter and adapt pathcorrection unittest to low momentum scenario. Fix interface of navigator state

--- core/include/detray/propagator/actors/aborters.hpp
+++ core/include/detray/propagator/actors/aborters.hpp
@@ -1,18 +1,19 @@
 /** Detray library, part of the ACTS project (R&D line)
  *
- * (c) 2022 CERN for the benefit of the ACTS project
+ * (c) 2022-2023 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
 
 #pragma once
 
-// detray definitions
-#include <climits>
-
+// Project include(s)
 #include "detray/definitions/qualifiers.hpp"
 #include "detray/propagator/base_actor.hpp"
 #include "detray/propagator/base_stepper.hpp"
+
+// System include(s)
+#include <limits>
 
 namespace detray {
 
@@ -59,6 +60,32 @@ struct pathlimit_aborter : actor {
         // Don't go over the path limit in the next step
         step_state.template set_constraint<step::constraint::e_aborter>(
             abrt_state.path_limit());
+    }
+};
+
+/// Aborter checks whether a specific surface was reached
+struct target_aborter : actor {
+
+    /// Keeps the index for the target surface
+    struct state {
+        /// Unique surface id of the target
+        dindex _target_surface_index{dindex_invalid};
+    };
+
+    /// Exits the navigation as soon as the target surface has been found.
+    ///
+    /// @param abrt_state contains the target surface index
+    /// @param prop_state state of the propagation
+    template <typename propagator_state_t>
+    DETRAY_HOST_DEVICE void operator()(const state &abrt_state,
+                                       propagator_state_t &prop_state) const {
+
+        auto &navigation = prop_state._navigation;
+
+        if (navigation.is_on_module() and
+            navigation.current_object() == abrt_state._target_surface_index) {
+            navigation.exit();
+        }
     }
 };
 
