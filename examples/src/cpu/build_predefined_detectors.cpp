bf44583fdad54cbe7c236190635b83576a55cf36
Joana Niermann <joana.niermann@cern.ch>
2023-05-12 22:58:09 +0200

    add bent telescope example

--- examples/src/cpu/build_predefined_detectors.cpp
+++ examples/src/cpu/build_predefined_detectors.cpp
@@ -9,7 +9,10 @@
 #include "detray/detectors/create_telescope_detector.hpp"
 #include "detray/detectors/create_toy_geometry.hpp"
 #include "detray/geometry/volume_graph.hpp"
+#include "detray/intersection/detail/trajectories.hpp"
 #include "detray/masks/masks.hpp"
+#include "detray/materials/predefined_materials.hpp"
+#include "detray/tracks/tracks.hpp"
 
 // Example linear algebra plugin: std::array
 #include "detray/examples/types.hpp"
@@ -66,12 +69,20 @@ int main() {
     using trapzd_telescope_t =
         detray::detector<detray::telescope_metadata<detray::trapezoid2D<>>>;
 
-    // Mask of a rectangle surfaces (20x20 mm)
+    // Mask with a rectangular shape (20x20 mm)
     detray::mask<detray::rectangle2D<>> rectangle{
         0u, 20.f * detray::unit<detray::scalar>::mm,
         20.f * detray::unit<detray::scalar>::mm};
 
-    // Build from given module positions
+    // Mask with a trapezoid shape
+    constexpr detray::scalar hx_min_y{10.f * detray::unit<detray::scalar>::mm};
+    constexpr detray::scalar hx_max_y{30.f * detray::unit<detray::scalar>::mm};
+    constexpr detray::scalar hy{20.f * detray::unit<detray::scalar>::mm};
+    constexpr detray::scalar divisor{10.f / (20.f * hy)};
+    detray::mask<detray::trapezoid2D<>> trapezoid{0u, hx_min_y, hx_max_y, hy,
+                                                  divisor};
+
+    // Build from given module positions (places 11 surfaces)
     std::vector<detray::scalar> positions = {
         0.f * detray::unit<detray::scalar>::mm,
         50.f * detray::unit<detray::scalar>::mm,
@@ -85,16 +96,57 @@ int main() {
         450.f * detray::unit<detray::scalar>::mm,
         500.f * detray::unit<detray::scalar>::mm};
 
+    //
     // Case 1: Defaults: Straight telescope in z-direction,
     //         10 rectangle surfaces, 500mm in length, modules evenly spaced,
-    //         no B-field
+    //         no B-field, silicon material (80mm)
     const rectgl_telescope_t tel_det1 =
         detray::create_telescope_detector(host_mr, rectangle);
     // std::cout << detray::volume_graph{tel_det1}.to_string() << std::endl;
 
+    //
     // Case 2: Straight telescope in z-direction, 100 trapezoid surfaces, 2000mm
-    //         in length, modules evenly spaced, no b-field
-    const rectgl_telescope_t tel_det2 = detray::create_telescope_detector(
-        host_mr, rectangle, 100, 2000.f * detray::unit<detray::scalar>::mm);
+    //         in length, modules evenly spaced, no B-field,
+    //         silicon material (80mm)
+    const trapzd_telescope_t tel_det2 = detray::create_telescope_detector(
+        host_mr, trapezoid, 100, 2000.f * detray::unit<detray::scalar>::mm);
     // std::cout << detray::volume_graph{tel_det2}.to_string() << std::endl;
+
+    //
+    // Case 3: Straight telescope in x-direction, 11 rectangle surfaces, 2000mm
+    //         in length, modules evenly spaced, no B-field,
+    //         silicon material (80mm)
+
+    // Pilot trajectory in x-direction
+    detray::detail::ray<detray::example::transform3> x_track{
+        {0.f, 0.f, 0.f}, 0.f, {1.f, 0.f, 0.f}, -1.f};
+
+    const rectgl_telescope_t tel_det3 = create_telescope_detector(
+        host_mr, rectangle, positions, detray::silicon_tml<detray::scalar>(),
+        80.f * detray::unit<detray::scalar>::um, x_track);
+    // std::cout << detray::volume_graph{tel_det3}.to_string() << std::endl;
+
+    //
+    // Case 4: Bent telescope along helical track, 11 trapezoid surfaces,
+    //         modules spaced according to given positions,
+    //         constant B-field, silicon material (80mm)
+
+    // Pilot trajectory in x-direction
+    detray::free_track_parameters<detray::example::transform3> y_track{
+        {0.f, 0.f, 0.f}, 0.f, {1.f, 0.f, 0.f}, -1.f};
+    // Helix in a constant B-field in z-direction
+    detray::example::vector3 B_z{0.f, 0.f,
+                                 1.f * detray::unit<detray::scalar>::T};
+    detray::detail::helix<detray::example::transform3> helix(y_track, &B_z);
+
+    // Prepare constant B-field
+    using b_field_t = typename trapzd_telescope_t::bfield_type;
+    b_field_t b_field_z{
+        b_field_t::backend_t::configuration_t{B_z[0], B_z[1], B_z[2]}};
+
+    const trapzd_telescope_t tel_det4 = detray::create_telescope_detector(
+        host_mr, std::move(b_field_z), trapezoid, positions,
+        detray::silicon_tml<detray::scalar>(),
+        80.f * detray::unit<detray::scalar>::um, helix);
+    // std::cout << detray::volume_graph{tel_det4}.to_string() << std::endl;
 }
