21c75a9c6b3bcdcdcf329e5696fc0e156c4381b8
Guilherme <g.almeida@cern.ch>
2023-06-01 10:46:21 +0200

    remove single store refactor

--- tests/unit_tests/device/cuda/tuple_types.cu
+++ tests/unit_tests/device/cuda/tuple_types.cu
@@ -10,7 +10,6 @@
 #include <vecmem/memory/host_memory_resource.hpp>
 #include <vecmem/utils/cuda/copy.hpp>
 
-#include "detray/core/detail/single_store copy.hpp"
 #include "detray/core/detail/tuple_container copy.hpp"
 #include "detray/definitions/cuda_definitions.hpp"
 
@@ -22,54 +21,6 @@
 using namespace detray;
 
 constexpr float tol{1e-6f};
-
-using types = single_store_types<int>;
-
-__global__ void basic_kernel(types::view view) {
-    std::size_t globalIdx = threadIdx.x + blockIdx.x * blockDim.x;
-
-    if (globalIdx >= view.size()) {
-        return;
-    }
-
-    view.at(globalIdx) += globalIdx;
-}
-
-GTEST_TEST(detray_detail, single_store) {
-
-    types::host host(3, 3);
-    host.at(1) = 100;
-    host.push_back(-1);
-
-    types::view host_view(host.get_data());
-    host_view.at(2) -= 1;
-
-    EXPECT_EQ(host.size(), 4);
-    EXPECT_EQ(host.at(0), 3);
-    EXPECT_EQ(host.at(1), 100);
-    EXPECT_EQ(host.at(2), 2);
-    EXPECT_EQ(host.at(3), -1);
-
-    vecmem::host_memory_resource host_mr;
-    vecmem::cuda::device_memory_resource dev_mr;
-    vecmem::cuda::copy cpy;
-
-    types::buffer dev_buffer(host.get_data(), dev_mr, cpy);
-
-    types::view dev_view(dev_buffer.get_data());
-
-    basic_kernel<<<8, 1>>>(dev_view);
-
-    types::buffer host_buffer(dev_buffer.get_data(), host_mr, cpy);
-    types::view host_view_2(host_buffer.get_data());
-
-    EXPECT_EQ(host_view_2.size(), 4);
-    EXPECT_EQ(host_view_2.at(0), 3);
-    EXPECT_EQ(host_view_2.at(1), 101);
-    EXPECT_EQ(host_view_2.at(2), 4);
-    EXPECT_EQ(host_view_2.at(3), 2);
-}
-
 
 using t0 = vecmem_types<unsigned, true>; // vector of unsigned
 using t1 = vecmem_types<int, false>; // 1 int
