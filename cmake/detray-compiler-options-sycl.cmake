66554c264a3fdb1d4602628d051b6386936643e9
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2024-04-12 11:14:12 +0200

    Introduced DETRAY_FAIL_ON_WARNINGS.
    Instead of only using -Werror in debug builds, made its use
    configurable through this flag.
    
    At the same time simplified the SYCL build flag configuration
    a bit (which current VecMem versions now allow us to do), and
    made the CUDA flag configuration just a little more robust.

--- cmake/detray-compiler-options-sycl.cmake
+++ cmake/detray-compiler-options-sycl.cmake
@@ -1,6 +1,6 @@
 # TRACCC library, part of the ACTS project (R&D line)
 #
-# (c) 2023 CERN for the benefit of the ACTS project
+# (c) 2023-2024 CERN for the benefit of the ACTS project
 #
 # Mozilla Public License Version 2.0
 
@@ -8,24 +8,22 @@
 include( detray-functions )
 
 # Basic flags for all build modes.
-foreach( mode RELEASE RELWITHDEBINFO MINSIZEREL DEBUG )
-   detray_add_flag( CMAKE_SYCL_FLAGS_${mode} "-Wall" )
-   detray_add_flag( CMAKE_SYCL_FLAGS_${mode} "-Wextra" )
-   detray_add_flag( CMAKE_SYCL_FLAGS_${mode} "-Wno-unknown-cuda-version" )
-   detray_add_flag( CMAKE_SYCL_FLAGS_${mode} "-Wshadow" )
-   detray_add_flag( CMAKE_SYCL_FLAGS_${mode} "-Wunused-local-typedefs" )
-endforeach()
-
-# More rigorous tests for the Debug builds.
-detray_add_flag( CMAKE_SYCL_FLAGS_DEBUG "-Werror" )
+detray_add_flag( CMAKE_SYCL_FLAGS "-Wall" )
+detray_add_flag( CMAKE_SYCL_FLAGS "-Wextra" )
+detray_add_flag( CMAKE_SYCL_FLAGS "-Wno-unknown-cuda-version" )
+detray_add_flag( CMAKE_SYCL_FLAGS "-Wshadow" )
+detray_add_flag( CMAKE_SYCL_FLAGS "-Wunused-local-typedefs" )
 if( NOT WIN32 )
-   detray_add_flag( CMAKE_SYCL_FLAGS_DEBUG "-pedantic" )
+   detray_add_flag( CMAKE_SYCL_FLAGS "-pedantic" )
 endif()
 
 # Avoid issues coming from MSVC<->DPC++ argument differences.
 if( "${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC" )
-   foreach( mode RELEASE RELWITHDEBINFO MINSIZEREL DEBUG )
-      detray_add_flag( CMAKE_SYCL_FLAGS_${mode}
-         "-Wno-unused-command-line-argument" )
-   endforeach()
+   detray_add_flag( CMAKE_SYCL_FLAGS
+      "-Wno-unused-command-line-argument" )
+endif()
+
+# Fail on warnings, if asked for that behaviour.
+if( DETRAY_FAIL_ON_WARNINGS )
+   detray_add_flag( CMAKE_SYCL_FLAGS "-Werror" )
 endif()
