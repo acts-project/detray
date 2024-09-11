6ee9ab027b7c90f751acc5a18fe16d1952c8ce07
Joana Niermann <joana.niermann@cern.ch>
2024-09-11 15:50:16 +0200

    Implement step tracer actor and combine it with track inspector

--- tests/include/detray/test/device/propagator_test.hpp
+++ tests/include/detray/test/device/propagator_test.hpp
@@ -20,6 +20,7 @@
 #include "detray/propagator/propagator.hpp"
 #include "detray/propagator/rk_stepper.hpp"
 #include "detray/simulation/event_generator/track_generators.hpp"
+#include "detray/test/common/utils/step_tracer.hpp"
 #include "detray/tracks/tracks.hpp"
 #include "detray/utils/inspectors.hpp"
 
@@ -74,54 +75,16 @@ struct propagator_test_config {
     propagation::config propagation;
 };
 
-template <template <typename...> class vector_t>
-struct track_inspector : actor {
-
-    struct state {
-
-        state(vecmem::memory_resource &resource)
-            : _path_lengths(&resource),
-              _positions(&resource),
-              _jac_transports(&resource) {}
-
-        DETRAY_HOST_DEVICE
-        state(vector_t<scalar> path_lengths, vector_t<point3_t> positions,
-              vector_t<free_matrix_t> jac_transports)
-            : _path_lengths(path_lengths),
-              _positions(positions),
-              _jac_transports(jac_transports) {}
-
-        vector_t<scalar> _path_lengths;
-        vector_t<point3_t> _positions;
-        vector_t<free_matrix_t> _jac_transports;
-    };
-
-    template <typename propagator_state_t>
-    DETRAY_HOST_DEVICE void operator()(
-        state &inspector_state, const propagator_state_t &prop_state) const {
-
-        const auto &navigation = prop_state._navigation;
-        const auto &stepping = prop_state._stepping;
-
-        // Check the track position, path length and Jacobian at every surface
-        if (navigation.is_on_module() || navigation.is_on_portal()) {
-            inspector_state._path_lengths.push_back(stepping.path_length());
-            inspector_state._positions.push_back(stepping().pos());
-            inspector_state._jac_transports.push_back(stepping._jac_transport);
-        }
-    }
-};
-
 // Assemble actor chain type
-using inspector_host_t = track_inspector<vecmem::vector>;
-using inspector_device_t = track_inspector<vecmem::device_vector>;
+using step_tracer_host_t = step_tracer<algebra_t, vecmem::vector>;
+using step_tracer_device_t = step_tracer<algebra_t, vecmem::device_vector>;
 using actor_chain_host_t =
-    actor_chain<tuple, inspector_host_t, pathlimit_aborter,
+    actor_chain<tuple, step_tracer_host_t, pathlimit_aborter,
                 parameter_transporter<algebra_t>,
                 pointwise_material_interactor<algebra_t>,
                 parameter_resetter<algebra_t>>;
 using actor_chain_device_t =
-    actor_chain<tuple, inspector_device_t, pathlimit_aborter,
+    actor_chain<tuple, step_tracer_device_t, pathlimit_aborter,
                 parameter_transporter<algebra_t>,
                 pointwise_material_interactor<algebra_t>,
                 parameter_resetter<algebra_t>>;
@@ -151,9 +114,7 @@ inline auto run_propagation_host(vecmem::memory_resource *mr,
                                  const propagation::config &cfg,
                                  covfie::field<bfield_bknd_t> &field,
                                  const vecmem::vector<track_t> &tracks)
-    -> std::tuple<vecmem::jagged_vector<scalar_t>,
-                  vecmem::jagged_vector<point3_t>,
-                  vecmem::jagged_vector<free_matrix_t>> {
+    -> vecmem::jagged_vector<detail::step_data<algebra_t>> {
 
     // Construct propagator from stepper and navigator
     using host_stepper_t =
@@ -167,20 +128,19 @@ inline auto run_propagation_host(vecmem::memory_resource *mr,
     propagator_host_t p{cfg};
 
     // Create vector for track recording
-    vecmem::jagged_vector<scalar_t> host_path_lengths(mr);
-    vecmem::jagged_vector<point3_t> host_positions(mr);
-    vecmem::jagged_vector<free_matrix_t> host_jac_transports(mr);
+    vecmem::jagged_vector<detail::step_data<algebra_t>> host_steps(mr);
 
     for (const auto &trk : tracks) {
 
         // Create the propagator state
-        inspector_host_t::state insp_state{*mr};
+        step_tracer_host_t::state tracer_state{*mr};
+        tracer_state.collect_only_on_surface(true);
         pathlimit_aborter::state pathlimit_state{cfg.stepping.path_limit};
         parameter_transporter<algebra_t>::state transporter_state{};
         pointwise_material_interactor<algebra_t>::state interactor_state{};
         parameter_resetter<algebra_t>::state resetter_state{};
         auto actor_states =
-            detray::tie(insp_state, pathlimit_state, transporter_state,
+            detray::tie(tracer_state, pathlimit_state, transporter_state,
                         interactor_state, resetter_state);
 
         typename propagator_host_t::state state(trk, field, det);
@@ -192,96 +152,63 @@ inline auto run_propagation_host(vecmem::memory_resource *mr,
         if (!p.propagate(state, actor_states)) {
             std::cout << state._navigation.inspector().to_string() << std::endl;
             throw std::runtime_error("Host propagation failed");
-        } else if (insp_state._path_lengths.empty() ||
-                   insp_state._positions.empty() ||
-                   insp_state._jac_transports.empty()) {
+        } else if (tracer_state.get_step_data().empty()) {
             std::cout << state._navigation.inspector().to_string() << std::endl;
             throw std::runtime_error(
                 "Host propagation did not record reference data correctly");
         }
 
-        // Record the step information
-        host_path_lengths.push_back(insp_state._path_lengths);
-        host_positions.push_back(insp_state._positions);
-        host_jac_transports.push_back(insp_state._jac_transports);
+        host_steps.push_back(std::move(tracer_state.get_step_data()));
     }
 
-    return std::make_tuple(std::move(host_path_lengths),
-                           std::move(host_positions),
-                           std::move(host_jac_transports));
+    return host_steps;
 }
 
 /// Compare the results between host and device propagation
 inline void compare_propagation_results(
-    const vecmem::jagged_vector<point3_t> &host_positions,
-    const vecmem::jagged_vector<point3_t> &device_positions,
-    const vecmem::jagged_vector<scalar_t> &host_path_lengths,
-    const vecmem::jagged_vector<scalar_t> &device_path_lengths,
-    const vecmem::jagged_vector<free_matrix_t> &host_jac_transports,
-    const vecmem::jagged_vector<free_matrix_t> &device_jac_transports) {
+    const vecmem::jagged_vector<detail::step_data<algebra_t>> &host_steps,
+    const vecmem::jagged_vector<detail::step_data<algebra_t>> &device_steps) {
 
     // Make sure the same number of tracks were tested on both backends
-    ASSERT_EQ(host_positions.size(), device_positions.size());
-    ASSERT_EQ(host_path_lengths.size(), device_path_lengths.size());
-    ASSERT_EQ(host_jac_transports.size(), device_jac_transports.size());
+    ASSERT_EQ(host_steps.size(), device_steps.size());
 
     // Compare the positions and pathlengths
-    for (unsigned int i = 0u; i < host_positions.size(); i++) {
+    for (unsigned int i = 0u; i < host_steps.size(); i++) {
 
-        EXPECT_TRUE(host_positions[i].size() > 0)
+        EXPECT_TRUE(host_steps[i].size() > 0u)
             << "No surfaces forund on track " << i;
 
         // Recorded as many positions as path lengths
-        EXPECT_EQ(host_positions[i].size(), host_path_lengths[i].size());
-        EXPECT_EQ(device_positions[i].size(), device_path_lengths[i].size());
+        EXPECT_EQ(host_steps[i].size(), device_steps[i].size());
 
-        // Number of recorded steps for host and device
-        EXPECT_EQ(host_path_lengths[i].size(), device_path_lengths[i].size());
-        EXPECT_EQ(host_positions[i].size(), device_positions[i].size());
-
-        for (unsigned int j = 0u; j < math::min(host_positions[i].size(),
-                                                device_positions[i].size());
-             j++) {
+        for (unsigned int j = 0u;
+             j < math::min(host_steps[i].size(), device_steps[i].size()); j++) {
 
             // Compare recorded path lengths along track
-            const scalar_t host_pl = host_path_lengths[i][j];
-            const scalar_t device_pl = device_path_lengths[i][j];
+            const auto &host_step = host_steps[i][j];
+            const auto &device_step = device_steps[i][j];
 
-            EXPECT_NEAR(host_pl, device_pl, host_pl * is_close)
+            EXPECT_NEAR(host_step.path_length, device_step.path_length,
+                        host_step.path_length * is_close)
                 << "ERROR: Path length at track " << i << " step " << j
                 << std::endl;
 
             // Compare recorded positions along track
-            const point3_t &host_pos = host_positions[i][j];
-            const point3_t &device_pos = device_positions[i][j];
+            const point3_t &host_pos = host_step.track_params.pos();
+            const point3_t &device_pos = device_step.track_params.pos();
 
-            auto relative_error =
-                static_cast<point3_t>(1. / host_pl * (host_pos - device_pos));
+            auto relative_error = static_cast<point3_t>(
+                1.f / host_step.path_length * (host_pos - device_pos));
 
             EXPECT_NEAR(getter::norm(relative_error), 0.f, is_close)
                 << "ERROR: Position at track " << i << " step " << j << ": ["
                 << host_pos[0] << ", " << host_pos[1] << ", " << host_pos[2]
                 << "] (host), [" << device_pos[0] << ", " << device_pos[1]
                 << ", " << device_pos[2] << "] (device)" << std::endl;
-            ;
-        }
-    }
 
-    // Compare the Jacobians
-    for (unsigned int i = 0u; i < host_jac_transports.size(); i++) {
-        // Number of recorded steps for host and device
-        EXPECT_EQ(host_jac_transports[i].size(),
-                  device_jac_transports[i].size());
-
-        for (unsigned int j = 0u;
-             j < math::min(host_jac_transports[i].size(),
-                           device_jac_transports[i].size());
-             j++) {
-
-            const free_matrix_t &host_J = host_jac_transports[i][j];
-            const free_matrix_t &device_J = device_jac_transports[i][j];
-
-            scalar_t pl = host_path_lengths[i][j];
+            // Compare the Jacobians
+            const free_matrix_t &host_J = host_step.jacobian;
+            const free_matrix_t &device_J = device_step.jacobian;
 
             for (std::size_t row = 0u; row < e_free_size; row++) {
                 for (std::size_t col = 0u; col < e_free_size; col++) {
@@ -292,7 +219,8 @@ inline void compare_propagation_results(
                     scalar_t device_val =
                         matrix_operator().element(device_J, row, col);
 
-                    ASSERT_NEAR((host_val - device_val) / pl, 0.f, is_close)
+                    ASSERT_NEAR((host_val - device_val) / host_step.path_length,
+                                0.f, is_close)
                         << "ERROR: matrix element mismatch at row " << row
                         << ", col " << col << std::endl;
                 }
--- tests/integration_tests/device/cuda/propagator_cuda_kernel.hpp
+++ tests/integration_tests/device/cuda/propagator_cuda_kernel.hpp
@@ -33,13 +33,10 @@ using inhom_bknd_t = covfie::backend::affine<covfie::backend::linear<
 
 /// Launch the propagation test kernel
 template <typename bfield_bknd_t, typename detector_t>
-void propagator_test(typename detector_t::view_type,
-                     const propagation::config &,
-                     covfie::field_view<bfield_bknd_t>,
-                     vecmem::data::vector_view<track_t> &,
-                     vecmem::data::jagged_vector_view<scalar> &,
-                     vecmem::data::jagged_vector_view<point3_t> &,
-                     vecmem::data::jagged_vector_view<free_matrix_t> &);
+void propagator_test(
+    typename detector_t::view_type, const propagation::config &,
+    covfie::field_view<bfield_bknd_t>, vecmem::data::vector_view<track_t> &,
+    vecmem::data::jagged_vector_view<detail::step_data<algebra_t>> &);
 
 /// Test function for propagator on the device
 template <typename bfield_bknd_t, typename detector_t>
@@ -47,10 +44,8 @@ inline auto run_propagation_device(
     vecmem::memory_resource *mr, const propagation::config &cfg,
     typename detector_t::view_type det_view,
     covfie::field_view<bfield_bknd_t> field_data, dvector<track_t> &tracks,
-    const vecmem::jagged_vector<point3_t> &host_positions)
-    -> std::tuple<vecmem::jagged_vector<scalar>,
-                  vecmem::jagged_vector<point3_t>,
-                  vecmem::jagged_vector<free_matrix_t>> {
+    const vecmem::jagged_vector<detail::step_data<algebra_t>> &host_steps)
+    -> vecmem::jagged_vector<detail::step_data<algebra_t>> {
 
     // Helper object for performing memory copies.
     vecmem::copy copy;
@@ -61,39 +56,27 @@ inline auto run_propagation_device(
     // Create vector buffer for track recording
     std::vector<std::size_t> sizes(tracks.size(), 0);
     std::vector<std::size_t> capacities;
-    for (auto &r : host_positions) {
+    for (auto &st : host_steps) {
         // Add a few more elements for security (in case the device finds more
         // surfaces)
-        capacities.push_back(r.size() + 10u);
+        capacities.push_back(st.size() + 10u);
     }
 
-    vecmem::data::jagged_vector_buffer<scalar> path_lengths_buffer(
-        capacities, *mr, nullptr, vecmem::data::buffer_type::resizable);
-    vecmem::data::jagged_vector_buffer<point3_t> positions_buffer(
-        capacities, *mr, nullptr, vecmem::data::buffer_type::resizable);
-    vecmem::data::jagged_vector_buffer<free_matrix_t> jac_transports_buffer(
-        capacities, *mr, nullptr, vecmem::data::buffer_type::resizable);
+    vecmem::data::jagged_vector_buffer<detail::step_data<algebra_t>>
+        steps_buffer(capacities, *mr, nullptr,
+                     vecmem::data::buffer_type::resizable);
 
-    copy.setup(path_lengths_buffer);
-    copy.setup(positions_buffer);
-    copy.setup(jac_transports_buffer);
+    copy.setup(steps_buffer);
 
     // Run the propagator test for GPU device
-    propagator_test<bfield_bknd_t, detector_t>(
-        det_view, cfg, field_data, tracks_data, path_lengths_buffer,
-        positions_buffer, jac_transports_buffer);
+    propagator_test<bfield_bknd_t, detector_t>(det_view, cfg, field_data,
+                                               tracks_data, steps_buffer);
 
-    vecmem::jagged_vector<scalar> device_path_lengths(mr);
-    vecmem::jagged_vector<point3_t> device_positions(mr);
-    vecmem::jagged_vector<free_matrix_t> device_jac_transports(mr);
+    vecmem::jagged_vector<detail::step_data<algebra_t>> steps(mr);
 
-    copy(path_lengths_buffer, device_path_lengths);
-    copy(positions_buffer, device_positions);
-    copy(jac_transports_buffer, device_jac_transports);
+    copy(steps_buffer, steps);
 
-    return std::make_tuple(std::move(device_path_lengths),
-                           std::move(device_positions),
-                           std::move(device_jac_transports));
+    return steps;
 }
 
 /// Test chain for the propagator
@@ -109,20 +92,18 @@ inline auto run_propagation_test(vecmem::memory_resource *mr, detector_t &det,
     vecmem::vector<track_t> tracks_device(tracks_host, mr);
 
     // Host propagation
-    auto &&[host_path_lengths, host_positions, host_jac_transports] =
+    auto host_steps =
         run_propagation_host(mr, det, cfg.propagation, field, tracks_host);
 
     // Device propagation (device backend specific implementation)
     covfie::field<device_bfield_bknd_t> device_field(field);
-    auto &&[device_path_lengths, device_positions, device_jac_transports] =
+    auto device_steps =
         run_propagation_device<device_bfield_bknd_t, detector_t>(
             mr, cfg.propagation, det_view, device_field, tracks_device,
-            host_positions);
+            host_steps);
 
     // Check the results
-    compare_propagation_results(host_positions, device_positions,
-                                host_path_lengths, device_path_lengths,
-                                host_jac_transports, device_jac_transports);
+    compare_propagation_results(host_steps, device_steps);
 }
 
 }  // namespace detray
