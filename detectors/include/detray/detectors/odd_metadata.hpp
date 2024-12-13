657a8279356b654337eccf246c9b07a64765feec
Joana Niermann <joana.niermann@cern.ch>
2024-12-13 16:27:15 +0100

    Update ITk metadata and add ODD specific metadata

--- detectors/include/detray/detectors/odd_metadata.hpp
+++ detectors/include/detray/detectors/odd_metadata.hpp
@@ -1,6 +1,6 @@
 /** Detray library, part of the ACTS project (R&D line)
  *
- * (c) 2023-2024 CERN for the benefit of the ACTS project
+ * (c) 2024 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -14,12 +14,10 @@
 #include "detray/definitions/detail/indexing.hpp"
 #include "detray/geometry/detail/surface_descriptor.hpp"
 #include "detray/geometry/mask.hpp"
-#include "detray/geometry/shapes/annulus2D.hpp"
 #include "detray/geometry/shapes/concentric_cylinder2D.hpp"
 #include "detray/geometry/shapes/rectangle2D.hpp"
 #include "detray/geometry/shapes/ring2D.hpp"
 #include "detray/materials/material_map.hpp"
-#include "detray/materials/material_slab.hpp"
 #include "detray/navigation/accelerators/brute_force_finder.hpp"
 #include "detray/navigation/accelerators/surface_grid.hpp"
 
@@ -29,12 +27,11 @@
 namespace detray {
 
 //
-// Detector
+// ODD Detector
 //
 
-/// Defines a detector that contains squares, trapezoids and a bounding portal
-/// box.
-struct itk_metadata {
+/// Defines a detector that is design to contain the ODD geometry
+struct odd_metadata {
 
     /// Define the algebra type for the geometry and navigation
     using algebra_type = ALGEBRA_PLUGIN<detray::scalar>;
@@ -46,47 +43,17 @@ struct itk_metadata {
     // Surface Primitives
     //
 
-    /// The mask types for the detector sensitive surfaces
-    using annulus = mask<annulus2D, nav_link>;
+    /// The mask types for the ODD sensitive surfaces
     using rectangle = mask<rectangle2D, nav_link>;
+    using trapezoid = mask<trapezoid2D, nav_link>;
     // Types for portals
     using cylinder_portal = mask<concentric_cylinder2D, nav_link>;
     using disc_portal = mask<ring2D, nav_link>;
 
-    //
-    // Material Description
-    //
-
-    /// The material types to be mapped onto the surfaces: Here homogeneous
-    /// material
-    using slab = material_slab<detray::scalar>;
-
-    // Cylindrical material map
-    template <typename container_t>
-    using cylinder_map_t =
-        material_map<concentric_cylinder2D, scalar, container_t>;
-
-    // Disc material map
-    template <typename container_t>
-    using disc_map_t = material_map<ring2D, scalar, container_t>;
-
-    // Rectangular material map
-    template <typename container_t>
-    using rectangular_map_t = material_map<rectangle2D, scalar, container_t>;
-
-    /// How to store and link transforms. The geometry context allows to resolve
-    /// the conditions data for e.g. module alignment
-    template <template <typename...> class vector_t = dvector>
-    using transform_store =
-        single_store<dtransform3D<algebra_type>, vector_t, geometry_context>;
-
-    /// Assign the mask types to the mask tuple container entries. It may be a
-    /// good idea to have the most common types in the first tuple entries, in
-    /// order to minimize the depth of the 'unrolling' before a mask is found
-    /// in the tuple
+    /// Assign the mask types to the mask tuple container entries.
     enum class mask_ids : std::uint_least8_t {
         e_rectangle2 = 0u,
-        e_annulus2 = 1u,
+        e_trapezoid2 = 1u,
         e_portal_cylinder2 = 2u,
         e_portal_ring2 = 3u,
     };
@@ -98,15 +65,30 @@ struct itk_metadata {
               template <typename...> class vector_t = dvector>
     using mask_store =
         regular_multi_store<mask_ids, empty_context, tuple_t, vector_t,
-                            rectangle, annulus, cylinder_portal, disc_portal>;
+                            rectangle, trapezoid, cylinder_portal, disc_portal>;
+
+    /// How to store and link transforms. The geometry context allows to resolve
+    /// the conditions data for e.g. module alignment
+    template <template <typename...> class vector_t = dvector>
+    using transform_store =
+        single_store<dtransform3D<algebra_type>, vector_t, geometry_context>;
+
+    //
+    // Material Description
+    //
+    // Cylindrical material map
+    template <typename container_t>
+    using cylinder_map_t =
+        material_map<concentric_cylinder2D, scalar, container_t>;
+
+    // Disc material map
+    template <typename container_t>
+    using disc_map_t = material_map<ring2D, scalar, container_t>;
 
     /// Similar to the mask store, there is a material store, which
     enum class material_ids : std::uint_least8_t {
         e_disc2_map = 0u,
-        e_annulus2_map = 0u,
         e_concentric_cylinder2_map = 1u,
-        e_reactangle2_map = 2u,
-        e_slab = 3u,  //< keep for the EF-tracking geometry
         e_none = 4u,
     };
 
@@ -114,33 +96,16 @@ struct itk_metadata {
     /// conditions data ( @c empty_context )
     template <template <typename...> class tuple_t = dtuple,
               typename container_t = host_container_types>
-    using material_store =
-        multi_store<material_ids, empty_context, tuple_t,
-                    grid_collection<disc_map_t<container_t>>,
-                    grid_collection<cylinder_map_t<container_t>>,
-                    grid_collection<rectangular_map_t<container_t>>,
-                    typename container_t::template vector_type<slab>>;
+    using material_store = multi_store < material_ids,
+          empty_context, tuple_t, grid_collection<disc_map_t<container_t>>,
+          grid_collection<cylinder_map_t<container_t>>;
 
     /// Surface descriptor type used for sensitives, passives and portals
-    /// It holds the indices to the surface data in the detector data stores
-    /// that were defined above
     using transform_link = typename transform_store<>::link_type;
     using mask_link = typename mask_store<>::single_link;
     using material_link = typename material_store<>::single_link;
-    /// Surface type used for sensitives, passives and portals
     using surface_type =
         surface_descriptor<mask_link, material_link, transform_link, nav_link>;
-
-    /// How to index the constituent objects in a volume
-    /// If they share the same index value here, they will be added into the
-    /// same acceleration data structure in every respective volume
-    enum geo_objects : std::uint_least8_t {
-        e_portal = 0,
-        e_passive = 0,
-        e_sensitive = 1,
-        e_size = 2,
-        e_all = e_size,
-    };
 
     /// The acceleration data structures live in another tuple that needs to be
     /// indexed correctly:
@@ -151,20 +116,26 @@ struct itk_metadata {
         e_default = e_brute_force,
     };
 
-    /// How a volume finds its constituent objects in the detector containers
-    /// In this case: One range for sensitive/passive surfaces, one for portals
-    using object_link_type =
-        dmulti_index<dtyped_index<accel_ids, dindex>, geo_objects::e_size>;
-
     /// The tuple store that hold the acceleration data structures for all
-    /// volumes. Every collection of accelerationdata structures defines its
-    /// own container and view type. Does not make use of conditions data
-    /// ( @c empty_context )
+    /// volumes.
     template <template <typename...> class tuple_t = dtuple,
               typename container_t = host_container_types>
     using accelerator_store =
         multi_store<accel_ids, empty_context, tuple_t,
                     brute_force_collection<surface_type, container_t>>;
+
+    /// How to index the constituent objects in a volume
+    enum geo_objects : std::uint_least8_t {
+        e_portal = 0,
+        e_passive = 0,
+        e_sensitive = 1,
+        e_size = 2,
+        e_all = e_size,
+    };
+
+    /// How a volume finds its constituent objects in the detector containers
+    using object_link_type =
+        dmulti_index<dtyped_index<accel_ids, dindex>, geo_objects::e_size>;
 
     /// Data structure that allows to find the current detector volume from a
     /// given position. Here: Uniform grid with a 3D cylindrical shape
