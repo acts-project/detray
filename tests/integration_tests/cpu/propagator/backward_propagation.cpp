94bd0010389f884357019345ffb0278a0482e10f
Beomki Yeo <beomki.yeo@gmail.com>
2025-02-12 13:40:51 -0800

    Add test

--- tests/integration_tests/cpu/propagator/backward_propagation.cpp
+++ tests/integration_tests/cpu/propagator/backward_propagation.cpp
@@ -19,6 +19,7 @@
 
 // Detray test include(s)
 #include "detray/test/utils/detectors/build_telescope_detector.hpp"
+#include "detray/test/utils/detectors/build_toy_detector.hpp"
 #include "detray/test/utils/types.hpp"
 
 // Vecmem include(s)
@@ -37,11 +38,11 @@ using vector3 = test::vector3;
 
 // Test class for the backward propagation
 // Input tuple: < std::vector<plane positions>, tolerance >
-class BackwardPropagation
+class BackwardPropagationTelescope
     : public ::testing::TestWithParam<std::tuple<std::vector<scalar>, scalar>> {
 };
 
-TEST_P(BackwardPropagation, backward_propagation) {
+TEST_P(BackwardPropagationTelescope, backward_propagation_telescope) {
 
     const scalar tol = std::get<1>(GetParam());
 
@@ -167,10 +168,101 @@ TEST_P(BackwardPropagation, backward_propagation) {
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    telescope, BackwardPropagation,
+    telescope, BackwardPropagationTelescope,
     ::testing::Values(
         std::make_tuple(std::vector<scalar>{0.f}, 1e-5f),
         std::make_tuple(std::vector<scalar>{0.f, 10.f}, 1e-3f),
         std::make_tuple(std::vector<scalar>{0.f, 10.f, 20.f, 30.f, 40.f, 50.f,
                                             60.f, 70.f, 80.f, 90.f, 100.f},
                         1e-2f)));
+
+GTEST_TEST(BackwardPropagationToy, BackwardPropagationToy) {
+
+    vecmem::host_memory_resource host_mr;
+
+    // Create toy detector
+    toy_det_config<test::scalar> toy_cfg{};
+    const auto [det, names] =
+        build_toy_detector<test_algebra>(host_mr, toy_cfg);
+
+    // Create b field
+    using bfield_t = bfield::const_field_t<scalar>;
+    vector3 B{0.f * unit<scalar>::T, 0.f * unit<scalar>::T,
+              1.f * unit<scalar>::T};
+    const bfield_t hom_bfield = bfield::create_const_field<scalar>(B);
+
+    using navigator_t = navigator<decltype(det)>;
+    using rk_stepper_t = rk_stepper<bfield_t::view_t, test_algebra>;
+    using actor_chain_t =
+        actor_chain<parameter_transporter<test_algebra>,
+                    pointwise_material_interactor<test_algebra>,
+                    parameter_resetter<test_algebra>>;
+    using propagator_t = propagator<rk_stepper_t, navigator_t, actor_chain_t>;
+
+    // Particle hypothesis
+    pdg_particle<scalar> ptc = muon<scalar>();
+
+    // Bound vector
+    bound_parameters_vector<test_algebra> bound_vector{};
+    bound_vector.set_theta(constant<scalar>::pi_2);
+    bound_vector.set_qop(ptc.charge() / (1.f * unit<scalar>::GeV));
+
+    // Bound covariance
+    auto bound_cov = matrix::identity<
+        typename bound_track_parameters<test_algebra>::covariance_type>();
+
+    // Bound track parameter
+    const bound_track_parameters<test_algebra> bound_param0(
+        geometry::barcode{}.set_index(0u), bound_vector, bound_cov);
+
+    // Actors
+    parameter_transporter<test_algebra>::state bound_updater{};
+    pointwise_material_interactor<test_algebra>::state interactor{};
+    parameter_resetter<test_algebra>::state rst{};
+
+    propagation::config prop_cfg{};
+    prop_cfg.stepping.rk_error_tol = 1e-4f * unit<float>::mm;
+    prop_cfg.navigation.overstep_tolerance = -100.f * unit<float>::um;
+    propagator_t p{prop_cfg};
+
+    // Forward state
+    propagator_t::state fw_state(bound_param0, hom_bfield, det,
+                                 prop_cfg.context);
+    fw_state.set_particle(ptc);
+    fw_state.do_debug = true;
+
+    // Run propagator
+    p.propagate(fw_state, detray::tie(bound_updater, interactor, rst));
+
+    // Print the debug stream
+    std::cout << "Forward" << std::endl;
+    std::cout << std::endl;
+    std::cout << fw_state.debug_stream.str() << std::endl;
+
+    // Bound state after propagation
+    const auto& bound_param1 = fw_state._stepping.bound_params();
+
+    // Check if the track reaches the final surface
+    /*
+    EXPECT_EQ(bound_param0.surface_link().volume(), 4095u);
+    EXPECT_EQ(bound_param0.surface_link().index(), 0u);
+    EXPECT_EQ(bound_param1.surface_link().volume(), 0u);
+    EXPECT_EQ(bound_param1.surface_link().id(), surface_id::e_sensitive);
+    */
+
+    // Backward state
+    propagator_t::state bw_state(bound_param1, hom_bfield, det,
+                                 prop_cfg.context);
+    bw_state.set_particle(ptc);
+    bw_state.do_debug = true;
+    bw_state._navigation.set_direction(navigation::direction::e_backward);
+    bw_state._navigation.set_volume(bound_param1.surface_link().volume());
+
+    // Run propagator
+    p.propagate(bw_state, detray::tie(bound_updater, interactor, rst));
+
+    // Print the debug stream
+    std::cout << "Backward" << std::endl;
+    std::cout << std::endl;
+    std::cout << bw_state.debug_stream.str() << std::endl;
+}
