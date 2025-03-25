e779d0081436c0ee53b1fed7e07896d2bdbfe7a1
wermos <63574588+wermos@users.noreply.github.com>
2025-03-25 22:15:13 +0530

    Removed some unnecessary comments.

--- plugins/algebra/fastor/include/detray/plugins/algebra/fastor_definitions.hpp
+++ plugins/algebra/fastor/include/detray/plugins/algebra/fastor_definitions.hpp
@@ -1,40 +1,6 @@
-// /** Detray library, part of the ACTS project (R&D line)
-//  *
-//  * (c) 2023 CERN for the benefit of the ACTS project
-//  *
-//  * Mozilla Public License Version 2.0
-//  */
-
-// #pragma once
-
-// // Algebra-Plugins include
-// #include "algebra/fastor_fastor.hpp"
-
-// #define __plugin algebra::fastor
-// #define ALGEBRA_PLUGIN fastor
-
-// namespace detray {
-
-// // Define scalar type
-// using scalar = DETRAY_CUSTOM_SCALARTYPE;
-
-// // Define namespace(s)
-// namespace getter = algebra::getter;
-// namespace vector = algebra::vector;
-// namespace matrix = algebra::matrix;
-
-// // Define matrix/vector operator
-// template <typename scalar_t>
-// using standard_matrix_operator =
-//     matrix::actor<scalar_t, matrix::determinant::preset0<scalar_t>,
-//                   matrix::inverse::preset0<scalar_t>>;
-
-// }  // namespace detray
-
-
 /** Detray library, part of the ACTS project (R&D line)
  *
- * (c) 2025 CERN for the benefit of the ACTS project
+ * (c) 2023-2025 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
