49955f91b732cbd67f4952ef6dd437f8656f105b
Joana Niermann <53186085+niermann999@users.noreply.github.com>
2025-04-07 07:16:28 +0000

    fix: remove overlaps and make envelopes larger (#962)
    Enlarges the portal envelope, similar to what is also used in the generic detector in ACTS. This will become even more important with alignment changes, when the contained surfaces move slightly. Also adds the portal envelope to the maximal detector radius to prevent an overlap of the endcap modules with the outer cylinder portals, since this can lead to skipped surfaces. Adding the same to inner radius does not seem to be necessary

--- tests/include/detray/test/utils/detectors/build_toy_detector.hpp
+++ tests/include/detray/test/utils/detectors/build_toy_detector.hpp
@@ -119,7 +119,7 @@ struct toy_det_config {
     // Radius of the innermost volume that contains the beampipe
     scalar_t m_beampipe_volume_radius{25.f * unit<scalar_t>::mm};
     // Envelope around the modules used by the cylinder portal generator
-    scalar_t m_portal_envelope{0.5f * unit<scalar_t>::mm};
+    scalar_t m_portal_envelope{2.f * unit<scalar_t>::mm};
     /// Configuration for the homogeneous material generator
     hom_material_config<scalar_t> m_material_config{};
     /// Put material maps on portals or use homogenous material on modules
@@ -908,7 +908,7 @@ inline auto add_endcap_detector(
 
             endcap_cfg.center(center_z)
                 .inner_radius(inner_radius)
-                .outer_radius(outer_radius);
+                .outer_radius(outer_radius - cfg.envelope());
 
             // Configure the portal factory
             cylinder_portal_config<scalar_t> portal_cfg{};
