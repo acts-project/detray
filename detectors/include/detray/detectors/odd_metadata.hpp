11ab3da7566fb13eceee122c14a54779798231bf
Joana Niermann <53186085+niermann999@users.noreply.github.com>
2025-11-24 15:04:29 +0100

    Update odd and itk metadata (#1090)
    Ensure that odd and itk metadata compile in traccc

--- detectors/include/detray/detectors/odd_metadata.hpp
+++ detectors/include/detray/detectors/odd_metadata.hpp
@@ -1,6 +1,6 @@
 /** Detray library, part of the ACTS project (R&D line)
  *
- * (c) 2024 CERN for the benefit of the ACTS project
+ * (c) 2022-2024 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -10,39 +10,33 @@
 // Project include(s)
 #include "detray/core/detail/multi_store.hpp"
 #include "detray/core/detail/single_store.hpp"
-#include "detray/definitions/detail/containers.hpp"
-#include "detray/definitions/detail/indexing.hpp"
-#include "detray/geometry/detail/surface_descriptor.hpp"
+#include "detray/definitions/algebra.hpp"
+#include "detray/definitions/containers.hpp"
+#include "detray/definitions/indexing.hpp"
 #include "detray/geometry/mask.hpp"
 #include "detray/geometry/shapes/concentric_cylinder2D.hpp"
 #include "detray/geometry/shapes/rectangle2D.hpp"
 #include "detray/geometry/shapes/ring2D.hpp"
 #include "detray/geometry/shapes/trapezoid2D.hpp"
+#include "detray/geometry/surface_descriptor.hpp"
 #include "detray/materials/material_map.hpp"
 #include "detray/navigation/accelerators/brute_force_finder.hpp"
 #include "detray/navigation/accelerators/surface_grid.hpp"
 
-// Linear algebra types
-#include "detray/definitions/detail/algebra.hpp"
-
 namespace detray {
 
-//
-// ODD Detector
-//
-
-/// Defines a detector type that is design to contain the ODD geometry
+/// Assembles the detector type. This metatdata contains all available types
 template <concepts::algebra algebra_t>
 struct odd_metadata {
 
     /// Define the algebra type for the geometry and navigation
     using algebra_type = algebra_t;
+    using scalar_t = dscalar<algebra_type>;
 
-    /// Portal link type between volumes
+    /// Mask-to-(next)-volume link (potentially switchable for SoA)
     using nav_link = std::uint_least16_t;
 
-    /// How to store and link transforms. The geometry context allows to resolve
-    /// the conditions data for e.g. module alignment
+    /// How to store coordinate transform matrices
     template <template <typename...> class vector_t = dvector>
     using transform_store =
         single_store<dtransform3D<algebra_type>, vector_t, geometry_context>;
@@ -51,19 +45,20 @@ struct odd_metadata {
     // Surface Primitives
     //
 
-    /// The mask types for the ODD sensitive surfaces
-    using rectangle = mask<rectangle2D, nav_link>;
-    using trapezoid = mask<trapezoid2D, nav_link>;
+    /// Mask types
+    using rectangle = mask<rectangle2D, algebra_type, nav_link>;
+    using trapezoid = mask<trapezoid2D, algebra_type, nav_link>;
+    using cylinder_portal = mask<concentric_cylinder2D, algebra_type, nav_link>;
+    using disc = mask<ring2D, algebra_type, nav_link>;
 
-    // Types for portals
-    using cylinder_portal = mask<concentric_cylinder2D, nav_link>;
-    using disc_portal = mask<ring2D, nav_link>;
-
-    /// Assign the mask types to the mask tuple container entries.
+    /// Give your mask types a name (needs to be consecutive and has to match
+    /// the types position in the mask store!)
     enum class mask_ids : std::uint_least8_t {
         e_rectangle2 = 0u,
         e_trapezoid2 = 1u,
+        e_cylinder2 = 2u,
         e_portal_cylinder2 = 2u,
+        e_ring2 = 3u,
         e_portal_ring2 = 3u,
     };
 
@@ -72,45 +67,54 @@ struct odd_metadata {
 
         switch (mid) {
             case mask_ids::e_rectangle2:
-                os << "e_rectangle2";
+                // e_portal_rectangle2 has same value (0u)
+                os << "e_rectangle2/e_portal_rectangle2";
                 break;
             case mask_ids::e_trapezoid2:
                 os << "e_trapezoid2";
                 break;
-            case mask_ids::e_portal_cylinder2:
-                os << "e_portal_cylinder2";
+            case mask_ids::e_cylinder2:
+                os << "e_cylinder2/e_portal_cylinder2";
                 break;
-            case mask_ids::e_portal_ring2:
-                os << "e_portal_ring2";
+            case mask_ids::e_ring2:
+                // e_portal_ring2 has same value (5u)
+                os << "e_ring2/e_portal_ring2";
                 break;
         }
         return os;
     }
 
-    /// This is the mask collections tuple (in the detector called 'mask store')
+    /// How to store masks
     template <template <typename...> class vector_t = dvector>
     using mask_store =
         regular_multi_store<mask_ids, empty_context, dtuple, vector_t,
-                            rectangle, trapezoid, cylinder_portal, disc_portal>;
+                            rectangle, trapezoid, cylinder_portal, disc>;
 
     //
     // Material Description
     //
 
-    // Cylindrical material map
-    template <typename container_t>
-    using cylinder_map_t =
-        material_map<concentric_cylinder2D, scalar, container_t>;
+    /// Material grid types (default: closed bounds, regular binning)
+    /// @{
 
-    // Disc material map
+    // Disc material grid
     template <typename container_t>
-    using disc_map_t = material_map<ring2D, scalar, container_t>;
+    using disc_map_t = material_map<algebra_type, ring2D, container_t>;
 
-    /// Define positions of ODD material types in detector material store
+    // Concentric cylindrical material grid
+    template <typename container_t>
+    using concentric_cylinder2_map_t =
+        material_map<algebra_type, concentric_cylinder2D, container_t>;
+
+    /// @}
+
+    /// Give your material types a name (needs to be consecutive and has to
+    /// match the types position in the mask store!)
     enum class material_ids : std::uint_least8_t {
+        // Material texture (grid) shapes
         e_concentric_cylinder2_map = 0u,
         e_disc2_map = 1u,
-        e_none = 3u,
+        e_none = 2u,
     };
 
     DETRAY_HOST inline friend std::ostream& operator<<(std::ostream& os,
@@ -121,7 +125,8 @@ struct odd_metadata {
                 os << "e_concentric_cylinder2_map";
                 break;
             case material_ids::e_disc2_map:
-                os << "e_disc2_map";
+                // e_annulus2_map has same value (1u)
+                os << "e_disc2_map/e_annulus2_map";
                 break;
             case material_ids::e_none:
                 os << "e_none";
@@ -130,19 +135,23 @@ struct odd_metadata {
         return os;
     }
 
-    /// How to store and link materials. The material does not make use of
-    /// conditions data ( @c empty_context )
+    /// How to store materials
     template <typename container_t = host_container_types>
     using material_store =
         multi_store<material_ids, empty_context, dtuple,
-                    grid_collection<cylinder_map_t<container_t>>,
+                    grid_collection<concentric_cylinder2_map_t<container_t>>,
                     grid_collection<disc_map_t<container_t>>>;
 
     //
-    // Acceleration Structures
+    // Acceleration structures
     //
 
-    // Surface grid definition: dynamic bin size
+    /// surface grid types (default boundaries: closed binning)
+    /// @TODO: Will we need the types for all grid configurations (binnning,
+    /// bin boundaries, serializers)?
+    /// @{
+
+    // surface grid definition: bin-content: darray<surface_type, 9>
     template <typename axes_t, typename bin_entry_t, typename container_t>
     using surface_grid_t =
         grid<algebra_type, axes_t, bins::dynamic_array<bin_entry_t>,
@@ -153,16 +162,17 @@ struct odd_metadata {
     using cylinder2D_sf_grid =
         surface_grid_t<axes<concentric_cylinder2D>, bin_entry_t, container_t>;
 
-    // Disc grid for the endcap layers
+    // disc grid for the endcap layers
     template <typename bin_entry_t, typename container_t>
     using disc_sf_grid = surface_grid_t<axes<ring2D>, bin_entry_t, container_t>;
 
-    /// The acceleration data structures live in another tuple that needs to be
-    /// indexed correctly:
+    /// @}
+
+    /// Acceleration data structures
     enum class accel_ids : std::uint_least8_t {
         e_brute_force = 0u,     // test all surfaces in a volume (brute force)
-        e_disc_grid = 1u,       // endcap
-        e_cylinder2_grid = 2u,  // barrel
+        e_cylinder2_grid = 1u,  // e.g. barrel layers
+        e_disc_grid = 2u,       // e.g. endcap layers
         e_default = e_brute_force,
     };
 
@@ -171,28 +181,27 @@ struct odd_metadata {
 
         switch (aid) {
             case accel_ids::e_brute_force:
-                // e_default has same value (0u)
-                os << "e_brute_force/e_default";
-                break;
-            case accel_ids::e_disc_grid:
-                os << "e_disc_grid";
+                os << "e_brute_force";
                 break;
             case accel_ids::e_cylinder2_grid:
                 os << "e_cylinder2_grid";
+                break;
+            case accel_ids::e_disc_grid:
+                os << "e_disc_grid";
                 break;
         }
         return os;
     }
 
-    /// Surface descriptor type used for sensitives, passives and portals
+    /// How to link to the entries in the data stores
     using transform_link = typename transform_store<>::link_type;
-    using mask_link = typename mask_store<>::single_link;
+    using mask_link = typename mask_store<>::range_link;
     using material_link = typename material_store<>::single_link;
+    /// Surface type used for sensitives, passives and portals
     using surface_type =
         surface_descriptor<mask_link, material_link, transform_link, nav_link>;
 
-    /// The tuple store that hold the acceleration data structures for all
-    /// volumes.
+    /// How to store the acceleration data structures
     template <typename container_t = host_container_types>
     using accelerator_store = multi_store<
         accel_ids, empty_context, dtuple,
@@ -204,13 +213,15 @@ struct odd_metadata {
     // Volume descriptors
     //
 
-    /// How to index the constituent objects in a volume
+    /// How to index the constituent objects (surfaces) in a volume
+    /// If they share the same index value here, they will be added into the
+    /// same acceleration data structure (brute force is always at 0)
     enum geo_objects : std::uint_least8_t {
-        e_portal = 0u,
-        e_passive = 0u,
-        e_sensitive = 1u,
-        e_size = 2u,
-        e_all = e_size,
+        e_portal = 0u,     // Brute force search
+        e_sensitive = 1u,  // Grid accelerated search (can be different types)
+        e_passive = 0u,    // Brute force search
+        e_size = 2u,     // Every volume holds two acceleration data structures
+        e_all = e_size,  // i.e. the brute force method and one grid type
     };
 
     DETRAY_HOST inline friend std::ostream& operator<<(std::ostream& os,
@@ -232,7 +243,8 @@ struct odd_metadata {
         return os;
     }
 
-    /// How a volume finds its constituent objects in the detector containers
+    /// How a volume links to the accelration data structures
+    /// In this case: One link for portals/passives and one sensitive surfaces
     using object_link_type =
         dmulti_index<dtyped_index<accel_ids, dindex>, geo_objects::e_size>;
 
@@ -240,10 +252,11 @@ struct odd_metadata {
     // Volume acceleration structure
     //
 
-    /// Uniform grid with a 3D cylindrical shape
+    /// Volume search grid
     template <typename container_t = host_container_types>
     using volume_finder =
-        grid<axes<cylinder3D, axis::bounds::e_open, axis::irregular,
+        grid<algebra_type,
+             axes<cylinder3D, axis::bounds::e_open, axis::irregular,
                   axis::regular, axis::irregular>,
              bins::single<dindex>, simple_serializer, container_t>;
 };
