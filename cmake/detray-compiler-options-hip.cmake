6293b5970d8ec686a7ada9aad5c22fb4e76e1185
Joana Niermann <joana.niermann@cern.ch>
2026-03-20 11:18:00 +0100

    Rearrange cmake list file

--- cmake/detray-compiler-options-hip.cmake
+++ cmake/detray-compiler-options-hip.cmake
@@ -1,6 +1,6 @@
 # Detray library, part of the ACTS project (R&D line)
 #
-# (c) 2021-2025 CERN for the benefit of the ACTS project
+# (c) 2021-2026 CERN for the benefit of the ACTS project
 #
 # Mozilla Public License Version 2.0
 
@@ -11,6 +11,9 @@ if(PROJECT_IS_TOP_LEVEL)
 
     #find HIP
     find_package(HIP REQUIRED)
+
+    set(CMAKE_HIP_ARCHITECTURES gfx1031)
+
     # Basic flags for all build modes.
     if(
         ("${CMAKE_HIP_PLATFORM}" STREQUAL "hcc")
