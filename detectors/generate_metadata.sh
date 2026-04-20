10713814468bca74f6e9d5c27265d0293b941463
Joana Niermann <joana.niermann@cern.ch>
2026-04-20 16:40:52 +0200

    Make metadata generation shell script more portable

--- detectors/generate_metadata.sh
+++ detectors/generate_metadata.sh
@@ -4,6 +4,8 @@
 #
 # Mozilla Public License Version 2.0
 
+#!/bin/sh
+
 # Print help message
 print_help()
 {
@@ -11,71 +13,102 @@ print_help()
    echo "Options:"
    echo "  m     Full path and filename of the python metadata script"
    echo "        in the \"detray/detectors/python\" directory."
-   echo "  b     Where to find the detray python setup script."
+   echo "  p     Where to find the detray python package (e.g. build/python)."
    echo "  o     Output directory."
    echo "  v     Verbose logging."
    echo "  vv    Debug level logging."
    echo "  h     Print this help."
    echo
+
+   return
 }
 
 # Verbosity level of the generator script
 log_lvl=0
 
 # Parse options
-while getopts ":hvb:vv:m:o:" opt; do
-    case $opt in
+while getopts ":hvp:vv:m:o:" opt; do
+    case ${opt} in
         h)
             echo "Generate custom detray Detector Metadata"
             echo
             print_help
-            exit
+            exit 0
         ;;
         v)
             ((log_lvl++))
         ;;
-        b)
-            build_dir=$OPTARG
+        p)
+            python_dir=${OPTARG}
         ;;
         m)
-            metadata_generator=$OPTARG
+            metadata_generator=${OPTARG}
         ;;
         o)
-            out_dir=$OPTARG
+            out_dir=${OPTARG}
         ;;
         \?)
-         echo
-         echo "ERROR: Invalid option $opt! Usage:"
-         echo
-         print_help
-         exit;;
+            >&2 echo
+            >&2 echo "ERROR: Invalid option ${opt}! Usage:"
+            >&2 echo
+            print_help
+            exit 1
+        ;;
+        *)
+            >&2 echo
+            >&2 echo "ERROR: Unknown option ${opt}! Usage:"
+            >&2 echo
+            print_help
+            exit 1
+        ;;
     esac
 done
 
-python_command="python $metadata_generator"
-
-if [[ -z "$metadata_generator" ]];then
-    echo
-    echo "ERROR: No detector metadata scrip supplied! Usage:"
-    echo
+if [ -z "${metadata_generator}" ];then
+    >&2 echo
+    >&2 echo "ERROR: No detector metadata scrip supplied! Usage:"
+    >&2 echo
     print_help
-    exit
+    exit 1
 fi
 
+if [ -z "${python_dir}" ];then
+    >&2 echo
+    >&2 echo "ERROR: Path to the detray python package not supplied! Usage:"
+    >&2 echo
+    print_help
+    exit 1
+fi
+
+# Check how to invoke the python interpreter
+python_command="python3"
+if command -v {$python_command} > /dev/null; then
+    # Try again
+    python_command="python"
+    if command -v {$python_command} > /dev/null; then
+        >&2 echo
+        >&2 echo "ERROR: No python command found: exiting"
+        >&2 echo
+        exit 2
+    fi
+fi
+
+generator_command="${python_command} ${metadata_generator}"
+
 # Configure verbosity
-if [[ $log_lvl == 1 ]]; then
-    python_command="$python_command -v"
-elif [[ $log_lvl == 2 ]]; then
-    python_command="$python_command -vv"
+if [ ${log_lvl} -eq 1 ]; then
+    generator_command="${generator_command} -v"
+elif [ ${log_lvl} -eq 2 ]; then
+    generator_command="${generator_command} -vv"
 fi
 
 # Add the option for the custom output location
-if [[ ! -z "$out_dir" ]]; then
-    python_command="$python_command -o $out_dir"
+if [ ! -z "${out_dir}" ]; then
+    generator_command="${generator_command} -o ${out_dir}"
 fi
 
-# Set uo the detray python package
-source $build_dir/python/setup.sh
+# Set up the detray python package
+export PYTHONPATH="${python_dir}/python":$PYTHONPATH
 
 # Run the generation of the requested metadata
-eval $python_command
+eval ${generator_command}
